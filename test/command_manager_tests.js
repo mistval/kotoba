eefa83268560eeeadc33eba210549991b56c500e
mistval <randallschmidt1@gmail.com>
2017-11-06 02:41:10 -0600

    fix tests broken by constructor argument change
    Former-commit-id: 64a5b8e0664abedccefd6a36a853f1fdc4acba3b

--- test/command_manager_tests.js
+++ test/command_manager_tests.js
@@ -12,7 +12,7 @@ describe('CommandManager', function() {
   describe('Load', function() {
     it('Refuses to load the command and complains in the logger if there is a bad command', function() {
       let logger = new MockLogger();
-      let commandManager = new CommandManager(__dirname + '/mock_commands/invalid_and_valid', null, logger);
+      let commandManager = new CommandManager(__dirname + '/mock_commands/invalid_and_valid', null, logger, MockConfig);
       return commandManager.load(undefined, MockConfig).then(() => {
         assert(logger.failed === true);
         let invokeResult = commandManager.processInput(null, MsgHelpCommand, config);
@@ -21,7 +21,7 @@ describe('CommandManager', function() {
     });
     it('Loads good commands even if it encounters a bad one', function() {
       let logger = new MockLogger();
-      let commandManager = new CommandManager(__dirname + '/mock_commands/invalid_and_valid', null, logger);
+      let commandManager = new CommandManager(__dirname + '/mock_commands/invalid_and_valid', null, logger, MockConfig);
       return commandManager.load(undefined, MockConfig).then(() => {
         let invokeResult = commandManager.processInput(null, MsgAboutCommand, config);
         assert(invokeResult === true);
@@ -29,7 +29,7 @@ describe('CommandManager', function() {
     });
     it('Refuses to load command and complains if two commands have save uniqueId', function() {
       let logger = new MockLogger();
-      let commandManager = new CommandManager(__dirname + '/mock_commands/duplicate_unique_ids', null, logger);
+      let commandManager = new CommandManager(__dirname + '/mock_commands/duplicate_unique_ids', null, logger, MockConfig);
       return commandManager.load(undefined, MockConfig).then(() => {
         let invokeResult1 = commandManager.processInput(null, MsgAboutCommand, config);
         let invokeResult2 = commandManager.processInput(null, MsgHelpCommand, config);
@@ -39,7 +39,7 @@ describe('CommandManager', function() {
     });
     it('Refuses to load command and complains if two commands have the same alias', function() {
       let logger = new MockLogger();
-      let commandManager = new CommandManager(__dirname + '/mock_commands/duplicate_aliases', null, logger);
+      let commandManager = new CommandManager(__dirname + '/mock_commands/duplicate_aliases', null, logger, MockConfig);
       return commandManager.load(undefined, MockConfig).then(() => {
         let invokeResult1 = commandManager.processInput(null, MsgAboutCommand, config);
         let invokeResult2 = commandManager.processInput(null, MsgHelpCommand, config);
@@ -49,14 +49,14 @@ describe('CommandManager', function() {
     });
     it('Errors trying to load commands from nonexistent directory', function() {
       let logger = new MockLogger();
-      let commandManager = new CommandManager(__dirname + '/nonexistent_directory', null, logger);
+      let commandManager = new CommandManager(__dirname + '/nonexistent_directory', null, logger, MockConfig);
       return commandManager.load(undefined, MockConfig).then(() => {
         assert(logger.failed === true);
       });
     });
     it('Gracefully handles command that throws', function() {
       let logger = new MockLogger();
-      let commandManager = new CommandManager(__dirname + '/mock_commands/valid_throws', null, logger);
+      let commandManager = new CommandManager(__dirname + '/mock_commands/valid_throws', null, logger, MockConfig);
       return commandManager.load(undefined, MockConfig).then(() => {
         commandManager.processInput(null, MsgAboutCommand, config);
         assert(logger.failed === true);
