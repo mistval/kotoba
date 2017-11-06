0a6be657ae4b0e01d18e9c9ce8e27c02c2ab50d8
mistval <randallschmidt1@gmail.com>
2017-11-06 07:31:19 -0600

    remove now-unused code

--- core/implementations/persistence_implementation.js
+++ core/implementations/persistence_implementation.js
@@ -5,7 +5,6 @@ const storage = require('./../util/node_persist_atomic.js');
 const USER_DATA_KEY_PREFIX = 'User';
 const SERVER_DATA_KEY_PREFIX = 'Server';
 const GLOBAL_DATA_KEY = 'Global';
-const ALLOWED_CHANNELS_FOR_COMMAND_KEY = 'AllowedChannelsForCommand';
 
 function getData(key) {
   return storage.getItem(key).then(data => {
@@ -47,23 +46,6 @@ class PersistenceImplementation {
     return getData(GLOBAL_DATA_KEY);
   }
 
-  static getAllowedChannelsForCommand(msg, commandId, persistenceState) {
-    if (!msg.channel.guild) {
-      return Promise.reject(new Error('Trying to get allowed channels in a DM channel. Can\t do that.'));
-    }
-
-    return PersistenceImplementation.getDataForServer(msg.channel.guild.id, persistenceState).then(data => {
-      if (data) {
-        let allowedChannelsForCommand = data[ALLOWED_CHANNELS_FOR_COMMAND_KEY];
-        if (allowedChannelsForCommand) {
-          return allowedChannelsForCommand[commandId];
-        }
-      }
-
-      return;
-    });
-  }
-
   static editDataForUser(userId, editDataFunction, persistenceState) {
     let key = keyForUserId(userId);
     return editData(key, editDataFunction);
@@ -76,28 +58,6 @@ class PersistenceImplementation {
 
   static editGlobalData(editDataFunction, persistenceState) {
     return editData(GLOBAL_DATA_KEY, editDataFunction);
-  }
-
-  static editAllowedChannelsForCommand(msg, commandId, editFunction, persistenceState) {
-    if (!msg.channel.guild) {
-      let errorMessage = 'Trying to set allowed channels in a DM channel. Can\t do that.';
-      return Promise.reject(new Error(errorMessage));
-    }
-
-    return PersistenceImplementation.editDataForServer(
-      msg.channel.guild.id,
-      data => {
-        if (!data[ALLOWED_CHANNELS_FOR_COMMAND_KEY]) {
-          data[ALLOWED_CHANNELS_FOR_COMMAND_KEY] = {};
-        }
-
-        let newAllowedChannels = editFunction(data[ALLOWED_CHANNELS_FOR_COMMAND_KEY][commandId]);
-        data[ALLOWED_CHANNELS_FOR_COMMAND_KEY][commandId] = newAllowedChannels;
-        return data;
-      },
-      persistenceState).then(data => {
-        return data[ALLOWED_CHANNELS_FOR_COMMAND_KEY][commandId];
-      });
   }
 }
 
