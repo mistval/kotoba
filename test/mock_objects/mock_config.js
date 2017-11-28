af177e4fee6489c7863ddec677b199b2a0358f03
mistval <randallschmidt1@gmail.com>
2017-11-28 08:06:26 -0600

    delete monochrome core files (since we use the npm package now)
    Former-commit-id: 763694a229ffeea8009a6902f872d4d53f889260

--- test/mock_objects/mock_config.js
+++
@@ -1,9 +0,0 @@
-class MockConfig {
-  constructor(serverAdminRole, botAdminIds) {
-    this.botAdminIds = botAdminIds;
-    this.serverAdminRoleName = serverAdminRole;
-    this.genericErrorMessage = 'Error';
-  }
-}
-
-module.exports = MockConfig;
