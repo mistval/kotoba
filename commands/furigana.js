c88dc56ca873ccdb4768fd92a623ab589323f72e
mistval <randallschmidt1@gmail.com>
2018-01-03 06:55:34 -0600

    manual merge commands from private repo

--- commands/furigana.js
+++ commands/furigana.js
@@ -15,8 +15,7 @@ module.exports = {
   usageExample: 'k!furigana 吾輩は猫である',
   action(bot, msg, suffix) {
     if (!suffix) {
-      msg.channel.createMessage('Say \'k!furigana [Japanese text]\' to render Japanese text with furigana!');
-      return;
+      throw PublicError.createWithCustomPublicMessage('Say \'k!furigana [Japanese text]\' to render Japanese text with furigana!', true, 'No suffix');
     }
     if (suffix.length > 200) {
       throw PublicError.createWithCustomPublicMessage('Two hundred characters or fewer please :)', true, 'Too long');
