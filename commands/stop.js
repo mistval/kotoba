ac8365dda7c06ca4d679f178af517dfa2f0d2c85
mistval <randallschmidt1@gmail.com>
2018-02-15 07:12:33 -0600

    make the stop command stop shiritori as well as quizzes
    Former-commit-id: 5f5c77cc557ade376e8a2f8cc564893a70c5ed58

--- commands/stop.js
+++ commands/stop.js
@@ -1,11 +1,16 @@
 'use strict'
 const reload = require('require-reload')(require);
 const quizManager = reload('./../kotoba/quiz_manager.js');
+const shiritoriManager = reload('./../kotoba/shiritori/shiritori_manager.js');
 
 module.exports = {
   commandAliases: ['k!endquiz', 'k!endtest', 'k!stop', 'k!quit', 'ｋ！ｑｓ'],
   canBeChannelRestricted: false,
   action(bot, msg, suffix) {
-    return quizManager.stopQuiz(msg.channel.id, msg.author.id);
+    let locationId = msg.channel.id;
+    let userId = msg.author.id;
+    return Promise.resolve(quizManager.stopQuiz(locationId, userId)).then(() => {
+      return shiritoriManager.stop(locationId, userId);
+    });
   },
 };
