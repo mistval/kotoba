0b290762b96f4ef2d4e41ce1be3b682382b58f27
mistval <randallschmidt1@gmail.com>
2018-03-13 08:08:02 -0500

    apply the new style to the about page

--- webserver/static/about.js
+++ webserver/static/about.js
@@ -1,7 +1,4 @@
 $(function () {
-    $('#contact-form').validator();
-
-
     // when the form is submitted
     $('#contact-form').on('submit', function (e) {
 
@@ -16,19 +13,17 @@ $(function () {
                 data: $(this).serialize(),
                 success: function (data)
                 {
-                    // we recieve the type of the message: success x danger and apply it to the 
+                    // we recieve the type of the message: success x danger and apply it to the
                     var messageAlert = 'alert-' + data.type;
                     var messageText = data.message;
 
                     // let's compose Bootstrap alert box HTML
                     var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable col-md-8"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                    
+
                     // If we have messageAlert and messageText
                     if (messageAlert && messageText) {
                         // inject the alert to .messages div in our form
                         $('#contact-form').find('.messages').html(alertBox);
-                        // empty the form
-                        $('#contact-form')[0].reset();
                     }
                 },
                 error: function(error) {
