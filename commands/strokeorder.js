eb8c700530234c632f90d24f649c4d5a5735a6e8
Randall Schmidt <randallschmidt1@gmail.com>
2018-04-04 17:25:38 -0500

    style improvements to stroke order command
    Former-commit-id: db1f574f06e128bda2b4c8b4e454d10422869341

--- commands/strokeorder.js
+++ commands/strokeorder.js
@@ -1,20 +1,8 @@
-
 const reload = require('require-reload')(require);
 
 const jishoSearch = reload('./../kotoba/jisho_search.js');
-const navigationManager = reload('monochrome-bot').navigationManager;
-const constants = require('./../kotoba/constants.js');
-
-const PublicError = reload('monochrome-bot').PublicError;
-
-function createTitleOnlyEmbed(title) {
-  return {
-    embed: {
-      title,
-      color: constants.EMBED_NEUTRAL_COLOR,
-    },
-  };
-}
+const { navigationManager } = reload('monochrome-bot');
+const errors = reload('./../kotoba/util/errors.js');
 
 module.exports = {
   commandAliases: ['k!strokeorder', 'k!so'],
@@ -24,10 +12,17 @@ module.exports = {
   shortDescription: 'Search for details about a kanji\'s strokes.',
   longDescription: 'Search for details about a kanji\'s strokes. For most kanji, I will provide a sequential stroke order diagram from Jisho and a stroke order gif generated from KanjiVG data. If you enter more than one character, I\'ll show results for all of them.',
   usageExample: 'k!strokeorder 少',
-  action(bot, msg, suffix) {
+  action: async function action(bot, msg, suffix) {
     if (!suffix) {
-      throw PublicError.createWithCustomPublicMessage('Say \'k!strokeorder [kanji]\' to search for stroke order information. For example: k!strokeorder 瞬間', false, 'No suffix');
+      return errors.throwPublicErrorInfo('Stroke order', 'Say **k!strokeorder yourkanjihere** to search for stroke order information. For example: **k!strokeorder 瞬間**', 'No suffix');
     }
-    return jishoSearch.createNavigationForStrokeOrder(msg.author.username, msg.author.id, suffix).then(navigation => navigationManager.register(navigation, 6000000, msg));
+
+    const navigation = await jishoSearch.createNavigationForStrokeOrder(
+      msg.author.username,
+      msg.author.id,
+      suffix,
+    );
+
+    return navigationManager.register(navigation, 6000000, msg);
   },
 };
