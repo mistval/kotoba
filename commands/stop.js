c07cd39ed0ed18ebf06ba9329a52efe9d4b5754c
Randall Schmidt <randallschmidt1@gmail.com>
2018-04-04 17:30:20 -0500

    style fixes and code improvements in ./commands/stop.js
    Former-commit-id: 9768556559471e5cb2c5e24a823aefe3ee16a8da

--- commands/stop.js
+++ commands/stop.js
@@ -1,4 +1,3 @@
-
 const reload = require('require-reload')(require);
 
 const quizManager = reload('./../kotoba/quiz/manager.js');
@@ -7,9 +6,13 @@ const shiritoriManager = reload('./../kotoba/shiritori/shiritori_manager.js');
 module.exports = {
   commandAliases: ['k!endquiz', 'k!endtest', 'k!stop', 'k!quit', 'ｋ！ｑｓ'],
   canBeChannelRestricted: false,
-  action(bot, msg, suffix) {
+  action(bot, msg) {
     const locationId = msg.channel.id;
     const userId = msg.author.id;
-    return Promise.resolve(quizManager.stopQuiz(locationId, userId)).then(() => shiritoriManager.stop(locationId, userId));
+
+    return Promise.all([
+      quizManager.stopQuiz(locationId, userId),
+      shiritoriManager.stop(locationId, userId),
+    ]);
   },
 };
