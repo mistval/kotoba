0b7a59d8ebc35133ec1e424056b0010dbc43701a
Randall Schmidt <randallschmidt1@gmail.com>
2018-04-22 12:43:04 -0500

    add commands for remapped quiz deck unique ids

---
+++ commands/get_unmapped_uniqueids.js
@@ -0,0 +1,29 @@
+const reload = require('require-reload')(require);
+
+const ScoreStorageUtils = reload('./../kotoba/quiz/score_storage_utils.js');
+const decksMetadata = reload('./../objects/quiz/decks.json');
+
+const deckUniqueIdMap = {};
+Object.keys(decksMetadata).forEach((deckName) => {
+  deckUniqueIdMap[decksMetadata[deckName].uniqueId] = true;
+});
+
+module.exports = {
+  commandAliases: ['}unmappeduids'],
+  botAdminOnly: true,
+  action: async function action(bot, msg) {
+    const allScores = await ScoreStorageUtils.getGlobalScores();
+    const unmappedUniqueIds = [];
+
+    allScores.forEach((score) => {
+      if (
+        !deckUniqueIdMap[score.deckId] &&
+        unmappedUniqueIds.indexOf(score.deckId) === -1
+      ) {
+        unmappedUniqueIds.push(score.deckId);
+      }
+    });
+
+    return msg.channel.createMessage(JSON.stringify(unmappedUniqueIds), null, msg);
+  },
+};
