fa62247f0ab4f6aac6846c565b2ca5130b62bc37
Randall Schmidt <randallschmidt1@gmail.com>
2018-04-22 18:06:52 -0500

    AirBnB style in ./kotoba/examples_content_creator.js

--- kotoba/examples_content_creator.js
+++ kotoba/examples_content_creator.js
@@ -1,12 +1,10 @@
-'use strict'
 const reload = require('require-reload')(require);
 const UnofficialJishoApi = require('unofficial-jisho-api');
-const KotobaUtils = reload('./utils.js');
-const { logger, PublicError } = reload('monochrome-bot').logger;
+
 const constants = reload('./constants.js');
 const { throwPublicErrorFatal } = reload('./util/errors.js');
 
-const searchForExamples = new UnofficialJishoApi().searchForExamples;
+const { searchForExamples } = new UnofficialJishoApi();
 
 const MAX_EXAMPLE_RESULTS = 4;
 const LONG_CUTOFF_IN_CHARS = 22;
@@ -17,61 +15,65 @@ class ExamplesResult {
   }
 
   parseResult(results) {
-    let longExamples = [];
-    let shortExamples = [];
+    const longExamples = [];
+    const shortExamples = [];
 
-    for (let result of results.results) {
+    results.results.forEach((result) => {
       if (result.kanji.length > LONG_CUTOFF_IN_CHARS) {
         longExamples.push(result);
       } else {
         shortExamples.push(result);
       }
-    }
+    });
 
-    this.query_ = results.query;
-    this.examples_ = shortExamples.concat(longExamples);
-    this.uri_ = results.uri;
+    this.query = results.query;
+    this.examples = shortExamples.concat(longExamples);
+    this.uri = results.uri;
   }
 
   toDiscordBotContent(pageIndex) {
-    if (this.examples_.length === 0) {
+    if (this.examples.length === 0) {
       return {
         pages: 0,
         embed: {
-          title: 'Didn\'t find any examples for the word or phrase: ' + this.query_,
-          description: `Did you search for **romaji**? Jisho's example search doesn't like romaji, try using hiragana and/or kanji!`,
+          title: `Didn't find any examples for the word or phrase: ${this.query}`,
+          description: 'Did you search for **romaji**? Jisho\'s example search doesn\'t like romaji, try using hiragana and/or kanji!',
           color: constants.EMBED_NEUTRAL_COLOR,
         },
       };
     }
 
-    let exampleStart = pageIndex * MAX_EXAMPLE_RESULTS;
-    if (exampleStart >= this.examples_.length) {
-      return;
-    }
-    let exampleEnd = Math.min((pageIndex * MAX_EXAMPLE_RESULTS) + MAX_EXAMPLE_RESULTS, this.examples_.length);
-
-    let fields = [];
-    for (let i = exampleStart; i < exampleEnd; ++i) {
-      let sentenceInformation = this.examples_[i];
-      fields.push({name: sentenceInformation.kanji, value: sentenceInformation.kana + '\n' + sentenceInformation.english})
+    const exampleStart = pageIndex * MAX_EXAMPLE_RESULTS;
+    if (exampleStart >= this.examples.length) {
+      return undefined;
     }
 
-    let numberOfPages = Math.floor((this.examples_.length / MAX_EXAMPLE_RESULTS) + 1);
-    if (this.examples_.length % MAX_EXAMPLE_RESULTS === 0) {
-      --numberOfPages;
+    const exampleEnd = Math.min(
+      (pageIndex * MAX_EXAMPLE_RESULTS) + MAX_EXAMPLE_RESULTS,
+      this.examples.length,
+    );
+
+    const fields = [];
+    for (let i = exampleStart; i < exampleEnd; i += 1) {
+      const sentenceInformation = this.examples[i];
+      fields.push({ name: sentenceInformation.kanji, value: `${sentenceInformation.kana}\n${sentenceInformation.english}` });
+    }
+
+    let numberOfPages = Math.floor((this.examples.length / MAX_EXAMPLE_RESULTS) + 1);
+    if (this.examples.length % MAX_EXAMPLE_RESULTS === 0) {
+      numberOfPages -= 1;
     }
 
     return {
       found: true,
       pages: numberOfPages,
       embed: {
-        title: 'Example results for ' + this.query_ + ` (page ${pageIndex + 1} of ${numberOfPages})`,
-        url: this.uri_,
-        fields: fields,
+        title: `Example results for ${this.query} (page ${pageIndex + 1} of ${numberOfPages})`,
+        url: this.uri,
+        fields,
         color: constants.EMBED_NEUTRAL_COLOR,
-      }
-    }
+      },
+    };
   }
 }
 
