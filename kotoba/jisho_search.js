337aa3699253e62cb3af4a541a5a6cffe72b25b6
Randall Schmidt <randallschmidt1@gmail.com>
2018-04-23 14:39:54 -0500

    partial AirBnB style and refactoring of core commands

--- kotoba/jisho_search.js
+++ kotoba/jisho_search.js
@@ -4,10 +4,8 @@ const reload = require('require-reload')(require);
 const jishoWordSearch = reload('./jisho_word_search.js');
 const kanjiContentCreator = reload('./kanji_search_content_creator.js');
 const strokeOrderContentCreator = reload('./stroke_order_content_creator.js');
-const NavigationPage = reload('monochrome-bot').NavigationPage;
-const NavigationChapter = reload('monochrome-bot').NavigationChapter;
+const { NavigationPage, NavigationChapter, Navigation } = reload('monochrome-bot');
 const examplesContentCreator = reload('./examples_content_creator.js');
-const Navigation = reload('monochrome-bot').Navigation;
 const constants = reload('./constants.js');
 const JishoDiscordContentFormatter = reload('./jisho_discord_content_formatter.js');
 
@@ -19,166 +17,217 @@ const JISHO_EMOTE = '🇯';
 
 function addFooter(authorName, content) {
   if (!content) {
-    return;
+    return undefined;
   }
-  content.embed.footer = {
+
+  const contentCopy = Object.assign({}, content);
+  contentCopy.embed = Object.assign({}, content.embed);
+
+  contentCopy.embed.footer = {
     icon_url: constants.FOOTER_ICON_URI,
     text: `${authorName} can use the reaction buttons below to see more information!`,
   };
+
+  return contentCopy;
+}
+
+class NavigationChapterInformation {
+  constructor(navigationChapter, hasMultiplePages, hasAnyPages) {
+    this.navigationChapter = navigationChapter;
+    this.hasMultiplePages = hasMultiplePages;
+    this.hasAnyPages = hasAnyPages;
+  }
 }
 
 class StrokeOrderDataSource {
   constructor(authorName, kanjis, isStandalone) {
-    if (typeof kanjis === typeof '') {
-      kanjis = [kanjis];
-    }
-    this.kanjis_ = kanjis;
-    this.authorName_ = authorName;
-    this.isStandalone_ = isStandalone;
+    this.kanjis = kanjis;
+    this.authorName = authorName;
+    this.isStandalone = isStandalone;
   }
 
+  // Nothing to do here, but we need the method due to
+  // interface contract.
+  // eslint-disable-next-line class-methods-use-this
   prepareData() {
   }
 
   getPageFromPreparedData(arg, pageIndex) {
-    if (pageIndex < this.kanjis_.length) {
-      return strokeOrderContentCreator.createContent(this.kanjis_[pageIndex]).then((content) => {
-        if (this.kanjis_.length > 1 || !this.isStandalone_) {
-          addFooter(this.authorName_, content);
+    if (pageIndex < this.kanjis.length) {
+      return strokeOrderContentCreator.createContent(this.kanjis[pageIndex]).then((content) => {
+        if (this.kanjis.length > 1 || !this.isStandalone) {
+          return new NavigationPage(addFooter(this.authorName, content));
         }
         return new NavigationPage(content);
       });
     }
+
+    return undefined;
   }
 }
 
 class KanjiDataSource {
   constructor(authorName, kanjis, isStandalone) {
-    if (typeof kanjis === typeof '') {
-      kanjis = [kanjis];
-    }
-    this.kanjis_ = kanjis;
-    this.authorName_ = authorName;
-    this.isStandalone_ = isStandalone;
+    this.kanjis = kanjis;
+    this.authorName = authorName;
+    this.isStandalone = isStandalone;
   }
 
+  // Nothing to do here, but we need the method due to
+  // interface contract.
+  // eslint-disable-next-line class-methods-use-this
   prepareData() {
-
   }
 
   getPageFromPreparedData(arg, pageIndex) {
-    if (pageIndex < this.kanjis_.length) {
-      return kanjiContentCreator.createContent(this.kanjis_[pageIndex]).then((content) => {
-        if (this.kanjis_.length > 1 || !this.isStandalone_) {
-          addFooter(this.authorName_, content);
+    if (pageIndex < this.kanjis.length) {
+      return kanjiContentCreator.createContent(this.kanjis[pageIndex]).then((content) => {
+        if (this.kanjis.length > 1 || !this.isStandalone) {
+          return new NavigationPage(addFooter(this.authorName, content));
         }
         return new NavigationPage(content);
       });
     }
-  }
-}
 
-class JishoWordDataSource {
-  constructor(authorName, dictionaryResponseData) {
-    this.dictionaryResponseData_ = dictionaryResponseData;
-    this.authorName_ = authorName;
-  }
-
-  prepareData() {
-  }
-
-  getPageFromPreparedData(arg, pageIndex) {
-    const responseContent = this.dictionaryResponseData_.toDiscordBotContent(true, pageIndex);
-    addFooter(this.authorName_, responseContent);
-    if (!responseContent) {
-      return;
-    }
-    return new NavigationPage(responseContent);
+    return undefined;
   }
 }
 
 class ExamplesSource {
-  constructor(authorName, word) {
-    this.word_ = word;
-    this.authorName_ = authorName;
+  constructor(authorName, word, isStandalone) {
+    this.word = word;
+    this.authorName = authorName;
+    this.isStandalone = isStandalone;
   }
 
   prepareData() {
-    return examplesContentCreator.createContent(this.word_);
+    return examplesContentCreator.createContent(this.word);
   }
 
   getPageFromPreparedData(arg, pageIndex) {
-    const content = arg.toDiscordBotContent(pageIndex);
-    const page = new NavigationPage(content);
-    if (content && content.pages > 1) {
-      addFooter(this.authorName_, content);
-    } else {
-      page.showPageArrows = false;
+    let content = arg.toDiscordBotContent(pageIndex);
+    let showPageArrows = false;
+    if ((content && content.pages > 1) || !this.isStandalone) {
+      content = addFooter(this.authorName, content);
+      showPageArrows = true;
     }
+
+    const page = new NavigationPage(content);
+    page.showPageArrows = showPageArrows;
+
     return page;
   }
 }
 
 function removeDuplicates(array) {
   if (!array) {
-    return;
+    return undefined;
   }
+
   return array.filter((element, i) => array.indexOf(element) === i);
 }
 
-function addNavigationChapterForKanji(authorName, word, chapterForEmojiName, isStandalone) {
+function createNavigationChapterInformationForKanji(authorName, word, isStandalone) {
   const kanjis = removeDuplicates(word.match(KANJI_REGEX));
+  const pageCount = kanjis ? kanjis.length : 0;
+  const hasMultiplePages = pageCount > 1;
+  const hasAnyPages = pageCount > 0;
+  let navigationChapter;
+
   if (kanjis && kanjis.length > 0) {
-    chapterForEmojiName[KANJI_EMOTE] = new NavigationChapter(new KanjiDataSource(authorName, kanjis, isStandalone));
+    navigationChapter = new NavigationChapter(new KanjiDataSource(
+      authorName,
+      kanjis,
+      isStandalone,
+    ));
   } else if (isStandalone) {
-    chapterForEmojiName[KANJI_EMOTE] = new NavigationChapter(new KanjiDataSource(authorName, word, isStandalone));
+    navigationChapter = new NavigationChapter(new KanjiDataSource(
+      authorName,
+      word,
+      isStandalone,
+    ));
   }
-  return kanjis ? kanjis.length : 0;
+
+  return new NavigationChapterInformation(navigationChapter, hasMultiplePages, hasAnyPages);
 }
 
-function addNavigationChapterForStrokeOrder(authorName, word, chapterForEmojiName, isStandalone) {
+function createNavigationChapterInformationForStrokeOrder(authorName, word, isStandalone) {
   const kanjis = removeDuplicates(word.match(KANJI_REGEX));
+  const pageCount = kanjis ? kanjis.length : 0;
+  const hasMultiplePages = pageCount > 1;
+  const hasAnyPages = pageCount > 0;
+  let navigationChapter;
+
   if (kanjis && kanjis.length > 0) {
-    chapterForEmojiName[STROKE_ORDER_EMOTE] = new NavigationChapter(new StrokeOrderDataSource(authorName, kanjis, isStandalone));
+    navigationChapter = new NavigationChapter(new StrokeOrderDataSource(
+      authorName,
+      kanjis,
+      isStandalone,
+    ));
   } else if (isStandalone) {
-    chapterForEmojiName[STROKE_ORDER_EMOTE] = new NavigationChapter(new StrokeOrderDataSource(authorName, word, isStandalone));
+    navigationChapter = new NavigationChapter(new StrokeOrderDataSource(
+      authorName,
+      word,
+      isStandalone,
+    ));
   }
 
-  return kanjis ? kanjis.length : 0;
+  return new NavigationChapterInformation(navigationChapter, hasMultiplePages, hasAnyPages);
 }
 
-function addNavigationChapterForExamples(authorName, word, chapterForEmojiName) {
-  chapterForEmojiName[EXAMPLES_EMOTE] = new NavigationChapter(new ExamplesSource(authorName, word));
+function createNavigationChapterInformationForExamples(authorName, word, isStandalone) {
+  const navigationChapter = new NavigationChapter(new ExamplesSource(
+    authorName,
+    word,
+    isStandalone,
+  ));
+
+  return new NavigationChapterInformation(navigationChapter);
 }
 
 function createNavigationForExamples(authorName, authorId, word) {
+  const navigationChapterInformation =
+    createNavigationChapterInformationForExamples(authorName, word, true);
+
   const chapterForEmojiName = {};
-  addNavigationChapterForExamples(authorName, word, chapterForEmojiName);
+  chapterForEmojiName[EXAMPLES_EMOTE] = navigationChapterInformation.navigationChapter;
   return Promise.resolve(new Navigation(authorId, true, EXAMPLES_EMOTE, chapterForEmojiName));
 }
 
 function createNavigationForStrokeOrder(authorName, authorId, kanji) {
-  const chapterForEmojiName = {};
-
-  const pageCount = addNavigationChapterForStrokeOrder(
+  const navigationChapterInformation = createNavigationChapterInformationForStrokeOrder(
     authorName,
     kanji,
-    chapterForEmojiName,
     true,
   );
 
+  const chapterForEmojiName = {};
+  chapterForEmojiName[STROKE_ORDER_EMOTE] = navigationChapterInformation.navigationChapter;
+
   return Promise.resolve(new Navigation(
     authorId,
-    pageCount > 1,
+    navigationChapterInformation.hasMultiplePages,
     STROKE_ORDER_EMOTE,
     chapterForEmojiName,
   ));
 }
 
 function createNavigationForKanji(authorName, authorId, kanji) {
+  const navigationChapterInformation = createNavigationChapterInformationForKanji(
+    authorName,
+    kanji,
+    true,
+  );
+
   const chapterForEmojiName = {};
-  const pageCount = addNavigationChapterForKanji(authorName, kanji, chapterForEmojiName, true);
-  return Promise.resolve(new Navigation(authorId, pageCount > 1, KANJI_EMOTE, chapterForEmojiName));
+  chapterForEmojiName[KANJI_EMOTE] = navigationChapterInformation.navigationChapter;
+
+  return Promise.resolve(new Navigation(
+    authorId,
+    navigationChapterInformation.hasMultiplePages,
+    KANJI_EMOTE,
+    chapterForEmojiName,
+  ));
 }
 
 function createNavigationForJishoResults(authorName, authorId, crossPlatformResponseData) {
@@ -190,12 +239,37 @@ function createNavigationForJishoResults(authorName, authorId, crossPlatformResp
     authorName,
   );
 
-  const navigationChapter = NavigationChapter.fromContent(discordContents);
+  const jishoNavigationChapter = NavigationChapter.fromContent(discordContents);
   const chapterForEmojiName = {};
-  chapterForEmojiName[JISHO_EMOTE] = navigationChapter;
-  addNavigationChapterForKanji(authorName, word, chapterForEmojiName);
-  addNavigationChapterForStrokeOrder(authorName, word, chapterForEmojiName);``
-  addNavigationChapterForExamples(authorName, word, chapterForEmojiName);
+  chapterForEmojiName[JISHO_EMOTE] = jishoNavigationChapter;
+
+  const kanjiNavigationChapterInformation = createNavigationChapterInformationForKanji(
+    authorName,
+    word,
+    false,
+  );
+  if (kanjiNavigationChapterInformation.hasAnyPages) {
+    chapterForEmojiName[KANJI_EMOTE] =
+      kanjiNavigationChapterInformation.navigationChapter;
+  }
+
+  const strokeOrderNavigationChapterInformation = createNavigationChapterInformationForStrokeOrder(
+    authorName,
+    word,
+    false,
+  );
+  if (strokeOrderNavigationChapterInformation.hasAnyPages) {
+    chapterForEmojiName[STROKE_ORDER_EMOTE] =
+      strokeOrderNavigationChapterInformation.navigationChapter;
+  }
+
+  const examplesNavigationChapter = createNavigationChapterInformationForExamples(
+    authorName,
+    word,
+    false,
+  ).navigationChapter;
+  chapterForEmojiName[EXAMPLES_EMOTE] = examplesNavigationChapter;
+
   return new Navigation(authorId, true, JISHO_EMOTE, chapterForEmojiName);
 }
 
