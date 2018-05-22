c2083bea4da8434b315659240e7120a6de0cf044
Randall Schmidt <randallschmidt1@gmail.com>
2018-05-22 02:07:09 -0500

    trim embed titles
    Former-commit-id: dba437b7b9fd4bc450f130791fb254abf2dc7512

---
+++ src/common/util/trim_embed.js
@@ -0,0 +1,44 @@
+const EMBED_FIELD_MAX_LENGTH = 1024;
+const EMBED_TITLE_MAX_LENGTH = 242; // Experimentally determined. It's supposed to be 256 but in practice it seems to not be >.>
+const EMBED_FIELD_TRIM_REPLACEMENT = ' [...]';
+
+function trimString(str, maxLength) {
+  if (str.length > maxLength) {
+    let trimmedStr = str.substring(
+      0,
+      maxLength - EMBED_FIELD_TRIM_REPLACEMENT.length,
+    );
+
+    trimmedStr += EMBED_FIELD_TRIM_REPLACEMENT;
+    return trimmedStr;
+  }
+
+  return str;
+}
+
+function trimEmbed(content) {
+  if (!content || !content.embed) {
+    return content;
+  }
+
+  const contentCopy = Object.assign({}, content);
+  contentCopy.embed = Object.assign({}, content.embed);
+  contentCopy.embed.title = trimString(contentCopy.embed.title, EMBED_TITLE_MAX_LENGTH);
+
+  if (!contentCopy.embed.fields || contentCopy.embed.fields.length === 0) {
+    return contentCopy;
+  }
+
+  const { fields } = contentCopy.embed;
+
+  contentCopy.embed.fields = fields.map((field) => {
+    const fieldCopy = Object.assign({}, field);
+    fieldCopy.value = trimString(fieldCopy.value, EMBED_FIELD_MAX_LENGTH);
+
+    return fieldCopy;
+  });
+
+  return contentCopy;
+}
+
+module.exports = trimEmbed;
