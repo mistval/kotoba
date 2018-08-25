6aa79b23190440b2000bc216f28129881ea3cf01
Randall Schmidt <randallschmidt1@gmail.com>
2018-08-24 21:15:13 -0500

    make it possible to set arbitrary options in the config.json file

--- check_sensitive_data.js
+++ check_sensitive_data.js
@@ -1,7 +1,6 @@
-const config = require('./config.json');
 const api_keys = require('./api_keys.js');
 
-if (config.botToken || Object.values(api_keys).some(value => !!value)) {
+if (Object.values(api_keys).some(value => !!value)) {
   console.log('Sensitive data found.');
   process.exit(1);
 }
