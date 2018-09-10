6f2ffd7a1f3ede5942bde41037ec0a3814faacaf
Randall Schmidt <randallschmidt1@gmail.com>
2018-09-10 03:02:53 -0500

    start refactoring shiritori to use a mongo db for storing words. the build script now puts the words in mongo

--- src/build/build_shiritori_data.js
+++ src/build/build_shiritori_data.js
@@ -3,6 +3,8 @@ const convertToHiragana = require('./../common/util/convert_to_hiragana.js');
 const fs = require('fs');
 const wordsByFrequency = require('./../../resources/dictionaries/frequency.json');
 const path = require('path');
+const wordDb = require('./../common/shiritori/jp_word_db.js');
+const mkdirp = require('mkdirp').sync;
 
 const partsOfSpeechPartRegex = /\((.*?)\)/;
 const edictNounCodes = [
@@ -15,132 +17,111 @@ const edictNounCodes = [
   'n-adv',
 ];
 
+const edictPath = path.resolve(
+  __dirname,
+  '..',
+  '..',
+  'resources',
+  'dictionaries',
+  'edictutf8.txt',
+);
+
 class WordInformation {
-  constructor(word, reading, definitions, isNoun) {
+  constructor(word, reading, definitions, isNoun, difficultyScore) {
     this.word = word;
     this.reading = reading;
     this.definitions = definitions;
     this.isNoun = isNoun;
+    this.difficultyScore = difficultyScore;
   }
-}
-
-function mkdirIgnoreError(dir) {
-  try {
-    fs.mkdirSync(dir);
-  } catch (err) {
-    // NOOP. Ignore error.
-  }
-}
-
-function getReadings(wordInformations) {
-  return wordInformations.map(wordInformation => wordInformation.reading);
 }
 
 function getEdictLines() {
-  const edictPath = path.resolve(
-    __dirname,
-    '..',
-    '..',
-    'resources',
-    'dictionaries',
-    'edictutf8.txt',
-  );
-
   const edictLines = fs.readFileSync(edictPath, 'utf8').split('\n');
   edictLines.shift(); // First line is a header.
   return edictLines;
 }
 
-function buildShiritoriData() {
-  const edictLines = getEdictLines();
-  const wordInformationIndicesForWordAsHiragana = {};
-  const wordInformations = [];
-
-  edictLines.forEach((line) => {
-    if (line) {
-      const tokens = line.split(' ');
-      const word = tokens.shift();
-      const wordAsHiragana = convertToHiragana(word);
-      const readingPart = tokens[0];
-      let reading;
-      if (readingPart.startsWith('[')) {
-        reading = convertToHiragana(readingPart.replace('[', '').replace(']', ''));
-        tokens.shift();
-      } else {
-        reading = wordAsHiragana;
-      }
-
-      const definitionParts = tokens.join(' ').split('/');
-      definitionParts.pop(); // The last one is always empty
-      definitionParts.shift(); // The first one is always empty
-      const definitions = [];
-      let isNoun = false;
-      definitionParts.forEach((definitionPart) => {
-        let definition = definitionPart;
-        let partOfSpeechMatch = definition.match(partsOfSpeechPartRegex);
-        while (partOfSpeechMatch) {
-          definition = definition.replace(partOfSpeechMatch[0], '');
-          const partsOfSpeech = partOfSpeechMatch[1].split(',');
-          if (!isNoun) {
-            isNoun = partsOfSpeech.some(partOfSpeechSymbol =>
-              edictNounCodes.indexOf(partOfSpeechSymbol) !== -1);
-          }
-          partOfSpeechMatch = definition.match(partsOfSpeechPartRegex);
-        }
-
-        definition = definition.trim();
-        if (definition) {
-          definitions.push(definition);
-        }
-      });
-
-      const wordInformation = new WordInformation(word, reading, definitions, isNoun);
-      wordInformations.push(wordInformation);
-      const wordIndex = wordInformations.length - 1;
-
-      if (!wordInformationIndicesForWordAsHiragana[wordAsHiragana]) {
-        wordInformationIndicesForWordAsHiragana[wordAsHiragana] = [];
-      }
-      wordInformationIndicesForWordAsHiragana[wordAsHiragana].push(wordIndex);
-      if (!wordInformationIndicesForWordAsHiragana[reading]) {
-        wordInformationIndicesForWordAsHiragana[reading] = [];
-      }
-      wordInformationIndicesForWordAsHiragana[reading].push(wordIndex);
-    }
-  });
-
-  const wordsForStartSequence = {};
-  wordsByFrequency.forEach((word) => {
-    const wordAsHiragana = convertToHiragana(word);
-    if (wordInformationIndicesForWordAsHiragana[wordAsHiragana]) {
-      const wordInformationIndices = wordInformationIndicesForWordAsHiragana[wordAsHiragana];
-      const wordInformationsForWord = wordInformationIndices.map(index => wordInformations[index]);
-      const readings = getReadings(wordInformationsForWord);
-      wordStartingSequences.forEach((startSequence) => {
-        if (
-          wordAsHiragana.startsWith(startSequence)
-          || (readings && readings.some(reading => reading.startsWith(startSequence)))
-        ) {
-          if (!wordsForStartSequence[startSequence]) {
-            wordsForStartSequence[startSequence] = [];
-          }
-          wordsForStartSequence[startSequence].push(word);
-        }
-      });
-    }
-  });
-
-  const outputData = {
-    wordInformationIndicesForWordAsHiragana,
-    wordsForStartSequence,
-    wordInformations,
-  };
-
-  mkdirIgnoreError(path.resolve(__dirname, '..', '..', 'generated'));
-  mkdirIgnoreError(path.resolve(__dirname, '..', '..', 'generated', 'shiritori'));
-
-  const filePath = path.resolve(__dirname, '..', '..', 'generated', 'shiritori', 'word_data.json');
-  fs.writeFileSync(filePath, JSON.stringify(outputData));
+function calculateDifficultyScore(reading) {
+  return 0;
 }
 
-module.exports = buildShiritoriData;
+async function build() {
+  console.log('-- Connecting to mongo DB');
+  await wordDb.connect();
+  console.log('-- Clearing JP word mongo DB');
+  await wordDb.clearWords();
+
+  const edictLines = getEdictLines();
+  let promises = [];
+  for (let i = 0; i < edictLines.length; ++i) {
+    const line = edictLines[i];
+
+    if (!line) {
+      continue;
+    }
+
+    const tokens = line.split(' ');
+    const word = tokens.shift();
+    const wordAsHiragana = convertToHiragana(word);
+    const readingPart = tokens[0];
+
+    let reading;
+    if (readingPart.startsWith('[')) {
+      reading = convertToHiragana(readingPart.replace('[', '').replace(']', ''));
+      tokens.shift();
+    } else {
+      reading = wordAsHiragana;
+    }
+
+    const definitionParts = tokens.join(' ').split('/');
+    definitionParts.pop(); // The last one is always empty
+    definitionParts.shift(); // The first one is always empty
+
+    const definitions = [];
+    let isNoun = false;
+    definitionParts.forEach((definitionPart) => {
+      let definition = definitionPart;
+      let partOfSpeechMatch = definition.match(partsOfSpeechPartRegex);
+      while (partOfSpeechMatch) {
+        definition = definition.replace(partOfSpeechMatch[0], '');
+        const partsOfSpeech = partOfSpeechMatch[1].split(',');
+        if (!isNoun) {
+          isNoun = partsOfSpeech.some(partOfSpeechSymbol =>
+            edictNounCodes.indexOf(partOfSpeechSymbol) !== -1);
+        }
+        partOfSpeechMatch = definition.match(partsOfSpeechPartRegex);
+      }
+
+      definition = definition.trim();
+      if (definition) {
+        definitions.push(definition);
+      }
+    });
+
+    const difficulty = calculateDifficultyScore(word);
+    const wordInformation = new WordInformation(
+      word,
+      reading,
+      definitions,
+      isNoun,
+      difficulty,
+    );
+
+    promises.push(wordDb.addWord(word, reading, definitions, isNoun, difficulty));
+
+    // To keep down memory usage
+    if (promises.length >= 1000) {
+      await Promise.all(promises);
+      promises = [];
+    }
+
+    if (i % 10000 === 0) {
+      console.log(`-- Words entered into DB: ${i}`);
+    }
+  }
+
+  return Promise.all(promises);
+}
+
+module.exports = build;
