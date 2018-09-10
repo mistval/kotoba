aae45f8a0135eca8499c64ae5c98923bd430eedb
Randall Schmidt <randallschmidt1@gmail.com>
2018-09-10 06:51:13 -0500

    get pronunciation command working from mongo

--- src/build/build.js
+++ src/build/build.js
@@ -1,7 +1,10 @@
 const buildQuizDecks = require('./build_quiz_decks.js');
-const buildShiritoriData = require('./build_shiritori_data.js')
+const buildShiritoriData = require('./build_shiritori_data.js');
+const buildPronounceData = require('./build_pronunciation_data.js');
 
 async function build() {
+  console.log('Building pronunciation data');
+  await buildPronounceData();
   console.log('Building shiritori data');
   await buildShiritoriData();
   console.log('Building quiz data');
---
+++ src/common/pronunciation_db.js
@@ -0,0 +1,42 @@
+const mongoConnect = require('./mongo_connect.js');
+const mongoose = require('mongoose');
+
+const pronunciationInfoSchema = new mongoose.Schema({
+  searchTerm: String,
+  words: [{
+    katakana: String,
+    pitchAccentClass: String,
+    pitchAccent: String,
+    kanji: [String],
+    noPronounceIndices: String,
+    nasalPitchIndices: String,
+  }],
+});
+
+PronunciationInfo = mongoose.model('PronunciationInfo', pronunciationInfoSchema);
+
+async function addEntry(searchTerm, words) {
+  await mongoConnect();
+  const entry = new PronunciationInfo({ searchTerm, words });
+  return entry.save();
+}
+
+async function search(searchTerm) {
+  await mongoConnect();
+  const searchResult = await PronunciationInfo.findOne({ searchTerm }).lean().exec();
+
+  if (searchResult) {
+    return searchResult.words;
+  }
+}
+
+async function clearPronunciationInfo() {
+  await mongoConnect();
+  return PronunciationInfo.deleteMany({});
+}
+
+module.exports = {
+  addEntry,
+  search,
+  clearPronunciationInfo,
+};
