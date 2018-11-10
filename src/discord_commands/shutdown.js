8aa82ecec44121aa404cbe5bb7a8800565b45954
mistval <randallschmidt1@gmail.com>
2018-11-10 17:04:34 +0800

    make the shutdown command safer

--- src/discord_commands/shutdown.js
+++ src/discord_commands/shutdown.js
@@ -2,14 +2,28 @@
 const reload = require('require-reload')(require);
 const PublicError = require('monochrome-bot').PublicError;
 
+function delay() {
+  return new Promise((fulfill, reject) => {
+    setTimeout(fulfill, 2000);
+  });
+}
+
 module.exports = {
   commandAliases: ['shutdown'],
   canBeChannelRestricted: false,
   botAdminOnly: true,
   uniqueId: 'shutdown',
-  action: async function action(bot, msg) {
+  action: async function action(bot, msg, suffix, monochrome) {
     await msg.channel.createMessage('Shutting down!');
-    bot.disconnect();
+
+    try {
+      await monochrome.stop();
+    } catch (err) {
+      console.warn('Error stopping');
+      console.warn(err);
+    }
+
+    await delay();
     process.exit(0);
   },
 };
