fcef475236cb69a8c8760d117ef6d41bb9a89c12
mistval <randallschmidt1@gmail.com>
2018-11-25 14:48:45 +0800

    remove the actual caching of the audio (I found out im not supposed to do that according to forvo's TOS)

--- src/common/forvo_cache.js
+++ src/common/forvo_cache.js
@@ -1,75 +1,11 @@
 const globals = require('./globals.js');
-const path = require('path');
-const fs = require('fs');
-const request = require('request');
 const reload = require('require-reload')(require);
 
 const forvoSearch = reload('./forvo_search.js');
 
-const CACHE_KEY = 'FORVO CACHE';
-const AUDIO_DIRECTORY = path.resolve(__dirname, '..', '..', 'generated', 'forvo_audio');
 const LOGGER_TITLE = 'FORVO CACHE';
 
-if (!fs.existsSync(AUDIO_DIRECTORY)) {
-  fs.mkdirSync(AUDIO_DIRECTORY);
-}
-
-function baseNamesToAbsolutePaths(baseNames) {
-  return baseNames.map(basename => path.join(AUDIO_DIRECTORY, basename));
-}
-
-// Return a concatenation of the string's char codes.
-function getStringCharCodeString(word) {
-  let charCodeString = '';
-  for (let i = 0; i < word.length; i += 1) {
-    const charCode = word.charCodeAt(i);
-    charCodeString = `${charCodeString}${charCode}`;
-  }
-
-  return charCodeString;
-}
-
-function downloadAndSaveFile(webUri, diskFilePath) {
-  // Apparently there's a memory leak in request-promise, so one should use
-  // the regular request library when streaming the response.
-  return new Promise((fulfill, reject) => {
-    const writeStream = fs.createWriteStream(diskFilePath);
-
-    writeStream.on('finish', () => {
-      fulfill();
-    });
-
-    request({
-      uri: webUri,
-      json: false,
-    },
-    (err) => {
-      if (err) {
-        reject(err)
-      }
-    }).pipe(writeStream);
-  });
-}
-
-function downloadAndSaveFiles(webUris, diskFilePaths) {
-  const promises = webUris
-    .map((webUri, index) => downloadAndSaveFile(webUri, diskFilePaths[index]));
-  return Promise.all(promises);
-}
-
 async function getPronunciationClipsForWord(word) {
-  const audioFileBasenamesForWord = await globals.persistence.getData(CACHE_KEY);
-  const audioFileBasenames = audioFileBasenamesForWord[word];
-
-  // If audio clips exist in the cache, return them.
-  if (audioFileBasenames !== undefined) {
-    globals.logger.logSuccess(LOGGER_TITLE, `Cache hit for (${word}) (${audioFileBasenames.join(', ')})`);
-    return baseNamesToAbsolutePaths(audioFileBasenames);
-  }
-
-  globals.logger.logFailure(LOGGER_TITLE, `Cache miss for (${word})`);
-
-  // If no audio clips exist in the cache, try to query Forvo.
   let forvoResponseData;
   try {
     forvoResponseData = await forvoSearch(word);
@@ -78,29 +14,10 @@ async function getPronunciationClipsForWord(word) {
     return [];
   }
 
-  // Download the audio clips from Forvo, record their location in the audioFileBasenamesForWord,
-  // and return the clip URIs.
-
   const audioClips = forvoResponseData.audioClips.filter(clipInfo => clipInfo.langname === 'Japanese');
   const audioClipWebUris = audioClips.map(clipInfo => clipInfo.audioUri);
 
-  const wordCharCodeString = getStringCharCodeString(word);
-  const audioClipDiskFileNames = audioClips.map((clipInfo, index) => `${wordCharCodeString}_${index}.${clipInfo.audioType}`);
-  const audioClipDiskFilePaths = baseNamesToAbsolutePaths(audioClipDiskFileNames);
-
-  const promises = [];
-
-  const recordResultPromise = globals.persistence.editData(CACHE_KEY, storedData => {
-    storedData[word] = audioClipDiskFileNames;
-    return storedData;
-  });
-
-  promises.push(recordResultPromise);
-  promises.push(downloadAndSaveFiles(audioClipWebUris, audioClipDiskFilePaths));
-
-  await Promise.all(promises);
-
-  return audioClipDiskFilePaths;
+  return audioClipWebUris;
 }
 
 module.exports = {
