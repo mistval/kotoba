ff531ee340c3680d2dbf305b5a0ef2b8eebafcf2
mistval <randallschmidt1@gmail.com>
2018-12-03 13:22:58 +0800

    simplify shutdown command
    Former-commit-id: bc0aa0e76029ad76499d2a642f496efda9583be4

--- src/discord_commands/shutdown.js
+++ src/discord_commands/shutdown.js
@@ -1,25 +1,10 @@
-function delay() {
-  return new Promise((fulfill) => {
-    setTimeout(fulfill, 2000);
-  });
-}
-
 module.exports = {
   commandAliases: ['shutdown'],
   canBeChannelRestricted: false,
   botAdminOnly: true,
   uniqueId: 'shutdown',
-  action: async function action(bot, msg, suffix, monochrome) {
+  action: async function action(bot, msg) {
     await msg.channel.createMessage('Shutting down!');
-
-    try {
-      await monochrome.stop();
-    } catch (err) {
-      console.warn('Error stopping');
-      console.warn(err);
-    }
-
-    await delay();
     process.exit(0);
   },
 };
