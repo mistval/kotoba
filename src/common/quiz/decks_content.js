b664bc0dfafa42a5a0e0d7f6d38bd9495cc4f41f
Randall Schmidt <randallschmidt1@gmail.com>
2018-12-13 19:04:36 +0700

    update available quiz decks

--- src/common/quiz/decks_content.js
+++ src/common/quiz/decks_content.js
@@ -10,12 +10,12 @@ const categoryFields = [
   {name: 'JLPT Kanji Usage Decks', value: 'k_N5 k_N4 k_N3 k_N2 k_N1'},
   {name: 'Kanken Kanji Reading Decks', value: '10k 9k 8k 7k 6k 5k 4k 3k j2k 2k j1k 1k'},
   {name: 'Listening Vocabulary Decks (audio)', value: 'lvN5 lvN4 lvN3 lvN2 lvN1 lv10k lv9k lv8k lv7k lv6k lv5k lv4k lv3k lvj2k lv2k lvj1k lv1k'},
-  {name: 'Japanese Proper Noun Decks', value: 'myouji namae onago seiyuu cities prefectures stations tokyo countries places kirakira'},
+  {name: 'Japanese Proper Noun Decks', value: 'myouji namae seiyuu cities prefectures countries'},
   {name: 'Mother Nature Decks (not for the faint of heart)', value: 'animals birds bugs fish plants vegetables'},
   {name: 'Kana Decks', value: 'hiragana katakana'},
-  {name: 'Japanese Misc. Decks', value: 'sao jptrivia common numbers hard haard insane yojijukugo kokuji onomato 擬音語 jpsyn jpsyn_hard jsyn jsynj ranobe radicals kklc jouyou images jpdefs'},
+  {name: 'Japanese Misc. Decks', value: 'sao jptrivia common numbers hard haard insane yojijukugo kokuji onomato 擬音語 ranobe radicals kklc jouyou images'},
   {name: 'Japanese Word Meaning Decks', value: 'N5m N4m N3m N2m N1m 10km 9km 8km 7km 6km 5km 4km 3km j2km 2km j1km 1km animalsm birdsm bugsm fishm plantsm vegetablesm hardm haardm commonm kokujim kklcm'},
-  {name: 'English Decks', value: 'defs1 defs2 defs3 (up to defs17)\nanagrams3 anagrams4 anagrams5 (up to anagrams10)\nejtrans ee en_syn'},
+  {name: 'English Decks', value: 'defs1 defs2 defs3 (up to defs17)\nanagrams3 anagrams4 anagrams5 (up to anagrams10)\nejtrans'},
   {name: 'Mixed Decks. The Grand Tour!', value: 'easymix medmix hardmix hardermix insanemix'},
 ];
 
