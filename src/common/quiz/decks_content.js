246c7afdad8deae0927ce61eabc1530c0e883ab4
Randall Schmidt <randallschmidt1@gmail.com>
2018-12-29 16:02:55 +0700

    update available quiz decks listing
    Former-commit-id: 9b4f17d36ed8ab1f786dff28c3f995e6ce780681

--- src/common/quiz/decks_content.js
+++ src/common/quiz/decks_content.js
@@ -4,16 +4,16 @@ const constants = reload('./../constants.js');
 
 const categoryFields = [
   {name: 'JLPT Kanji Reading Decks', value: 'N5 N4 N3 N2 N1'},
-  {name: 'JLPT Listening Decks (audio) (under construction)', value: 'LN3 LN2 LN1'},
-  {name: 'JLPT Grammar Decks', value: 'gN5 gN4 gN3 gN2 gN1'},
-  {name: 'JLPT Vocabulary Usage Decks', value: 'vuN3 vuN2 vuN1'},
+  {name: 'JLPT Grammar Decks', value: 'gN5 gN4 gN3 gN2 gN1 vuN3 vuN2 vuN1'},
   {name: 'JLPT Kanji Usage Decks', value: 'k_N5 k_N4 k_N3 k_N2 k_N1'},
+  {name: 'JLPT Listening Decks (audio) (under construction)', value: 'LN3 LN2 LN1'},
   {name: 'Kanken Kanji Reading Decks', value: '10k 9k 8k 7k 6k 5k 4k 3k j2k 2k j1k 1k'},
-  {name: 'Listening Vocabulary Decks (audio)', value: 'lvN5 lvN4 lvN3 lvN2 lvN1 lv10k lv9k lv8k lv7k lv6k lv5k lv4k lv3k lvj2k lv2k lvj1k lv1k'},
-  {name: 'Japanese Proper Noun Decks', value: 'myouji namae seiyuu cities prefectures countries'},
+  {name: 'Listening Vocabulary Decks (audio)', value: 'LVN5 LVN4 LVN3 LVN2 LVN1 LV10k LV9k LV8k LV7k LV6k LV5k LV4k LV3k LVj2k LV2k LVj1k LV1k'},
+  {name: '用語 decks', value: 'pasokon rikagaku kenchiku igaku '}
+  {name: 'Japanese Proper Noun Decks', value: 'myouji namae onago seiyuu cities prefectures countries'},
   {name: 'Mother Nature Decks (not for the faint of heart)', value: 'animals birds bugs fish plants vegetables'},
   {name: 'Kana Decks', value: 'hiragana katakana'},
-  {name: 'Japanese Misc. Decks', value: 'sao jptrivia common numbers hard haard insane yojijukugo kokuji onomato 擬音語 ranobe radicals kklc jouyou images'},
+  {name: 'Japanese Misc. Decks', value: 'kirakira sao jptrivia common numbers hard haard insane yojijukugo kokuji onomato 擬音語 ranobe radicals kklc jouyou images jpdefs'},
   {name: 'Japanese Word Meaning Decks', value: 'N5m N4m N3m N2m N1m 10km 9km 8km 7km 6km 5km 4km 3km j2km 2km j1km 1km animalsm birdsm bugsm fishm plantsm vegetablesm hardm haardm commonm kokujim kklcm'},
   {name: 'English Decks', value: 'defs1 defs2 defs3 (up to defs17)\nanagrams3 anagrams4 anagrams5 (up to anagrams10)\nejtrans'},
   {name: 'Mixed Decks. The Grand Tour!', value: 'easymix medmix hardmix hardermix insanemix'},
