aa725900649e7e3839963aecda8670b93b8e3fbf
Randall Schmidt <randallschmidt1@gmail.com>
2018-12-30 10:21:34 +0700

    add stations reading quizzes

--- src/common/quiz/decks_content.js
+++ src/common/quiz/decks_content.js
@@ -2,18 +2,19 @@
 const reload = require('require-reload')(require);
 const constants = reload('./../constants.js');
 
+const stationsList = ["stations_tokyo","stations_osaka","stations_hokaido","stations_fukuoka","stations_kyoto","stations_yamanashi","stations_nagano","stations_nigata","stations_aoyama","stations_iwate","stations_miyagi","stations_akita","stations_fukushima","stations_yamagata","stations_toyama","stations_ishikawa","stations_fukui","stations_kanagawa","stations_chiba","stations_saitama","stations_ibaraki","stations_gunma","stations_okayama","stations_hiroshima","stations_tottori","stations_shimane","stations_yamaguchi","stations_hyougo","stations_shiga","stations_nara","stations_wakayama","stations_shizuoka","stations_gifu","stations_mie","stations_saga","stations_nagasaki","stations_kumamoto","stations_oita","stations_miyazaki","stations_kagoshima","stations_tokushima","stations_kagawa","stations_ehime","stations_kochi","stations_okinawa"];
+
 const categoryFields = [
   {name: 'JLPT Kanji Reading Decks', value: 'N5 N4 N3 N2 N1'},
   {name: 'JLPT Grammar Decks', value: 'gN5 gN4 gN3 gN2 gN1 vuN3 vuN2 vuN1'},
   {name: 'JLPT Kanji Usage Decks', value: 'k_N5 k_N4 k_N3 k_N2 k_N1'},
   {name: 'JLPT Listening Decks (audio) (under construction)', value: 'LN3 LN2 LN1'},
   {name: 'Kanken Kanji Reading Decks', value: '10k 9k 8k 7k 6k 5k 4k 3k j2k 2k j1k 1k'},
-  {name: 'Listening Vocabulary Decks (audio)', value: 'LVN5 LVN4 LVN3 LVN2 LVN1 LV10k LV9k LV8k LV7k LV6k LV5k LV4k LV3k LVj2k LV2k LVj1k LV1k'},
-  {name: '用語 Decks', value: 'pasokon rikagaku igaku shinrigaku keizai houritsu kenchiku'},
+  {name: 'Listening Vocabulary Decks (audio)', value: 'lvN5 lvN4 lvN3 lvN2 lvN1 lv10k lv9k lv8k lv7k lv6k lv5k lv4k lv3k lvj2k lv2k lvj1k lv1k'},
+  {name: '用語 Decks', value: 'pasokon rikagaku igaku shinrigaku keizai houritsu kenchiku animals birds bugs fish plants vegetables'},
   {name: 'Japanese Proper Noun Decks', value: 'myouji namae onago seiyuu cities prefectures countries'},
-  {name: 'Mother Nature Decks (not for the faint of heart)', value: 'animals birds bugs fish plants vegetables'},
-  {name: 'Kana Decks', value: 'hiragana katakana'},
-  {name: 'Japanese Misc. Decks', value: 'kirakira sao jptrivia common numbers hard haard insane yojijukugo kokuji onomato 擬音語 ranobe radicals kklc jouyou images jpdefs'},
+  {name: 'Japanese Station Name Decks', value: 'stations_full stations_tokyo stations_osaka stations_hokaido stations_nagoya (say **<prefix>quiz stations** to see the full list)'},
+  {name: 'Japanese Misc. Decks', value: 'hiragana katakana kirakira sao jptrivia common numbers hard haard insane yojijukugo kokuji onomato 擬音語 ranobe radicals kklc jouyou images jpdefs'},
   {name: 'Japanese Word Meaning Decks', value: 'N5m N4m N3m N2m N1m 10km 9km 8km 7km 6km 5km 4km 3km j2km 2km j1km 1km animalsm birdsm bugsm fishm plantsm vegetablesm hardm haardm commonm kokujim kklcm'},
   {name: 'English Decks', value: 'defs1 defs2 defs3 (up to defs17)\nanagrams3 anagrams4 anagrams5 (up to anagrams10)\nejtrans'},
   {name: 'Mixed Decks. The Grand Tour!', value: 'easymix medmix hardmix hardermix insanemix'},
@@ -25,7 +26,7 @@ module.exports.createContent = function(prefix) {
       title: 'Quiz',
       description: `Say **${prefix}quiz deckname** to start a quiz (Example: **${prefix}quiz N5**). Say **${prefix}quiz stop** to stop a quiz. Say **nodelay** after the deck name for a lightning fast quiz, for example: **k!quiz N1 nodelay**. [Visit me on the web](http://kotobaweb.com/bot/quiz) to see advanced options and settings that you can use.`,
       color: constants.EMBED_NEUTRAL_COLOR,
-      fields: categoryFields,
+      fields: categoryFields.map(field => ({ name: field.name, value: field.value.replace(/<prefix>/g, prefix) })),
       footer: {
         icon_url: constants.FOOTER_ICON_URI,
         text: `You can mix any decks by using the + symbol. For example: ${prefix}quiz N5+N4+N3`,
@@ -34,24 +35,8 @@ module.exports.createContent = function(prefix) {
   };
 };
 
-module.exports.getHelpForCategory = function(userInput) {
-  userInput = userInput.toLowerCase();
-  for (let categoryField of categoryFields) {
-    let categoryName = categoryField.name.toLowerCase();
-    if (categoryName.startsWith(userInput)) {
-      let examples = 'You can start a quiz with the following commands:\n';
-      for (let deckName of categoryField.value.split(' ')) {
-        examples += '\nk!quiz ' + deckName;
-      }
-      let content = {
-        embed: {
-          title: categoryField.name,
-          description: examples,
-          color: constants.EMBED_NEUTRAL_COLOR,
-        },
-      };
-
-      return content;
-    }
+module.exports.getAdvancedHelp = function(keyword) {
+  if (keyword === 'stations') {
+    return `Stations reading quizzes \`\`\`${stationsList.join(', ')}\`\`\``;
   }
-}
+};
