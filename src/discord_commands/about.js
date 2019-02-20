d851cfeec0b6a9b2072d6ff90ab8740aafa74034
Randall Schmidt <randallschmidt1@gmail.com>
2019-02-20 21:28:44 +0700

    make the about command the way score wants it, closes #83
    Former-commit-id: e3f69dd677a14cb8893c6b5545857e3bd703ef93

--- src/discord_commands/about.js
+++ src/discord_commands/about.js
@@ -1,6 +1,14 @@
 const { erisVersion } = require('monochrome-bot');
 const monochromeVersion = require('monochrome-bot/package.json').version;
 
+const monochromeVersionString = `Monochrome v${monochromeVersion}`;
+const erisVersionString = `Eris v${erisVersion}`;
+const nameString = 'K33#5261';
+
+function createPadding(length, longestLength) {
+  return new Array(longestLength - length + 1).join(' ');
+}
+
 module.exports = {
   commandAliases: ['about'],
   uniqueId: 'about53463',
@@ -11,9 +19,9 @@ module.exports = {
     return msg.channel.createMessage(`\`\`\`md
 # Kotoba
 
-[ CREATOR ](K33#5261)
-[ LIBRARY ](Eris v${erisVersion})
-[ FRAMEWORK ](Monochrome v${monochromeVersion})
+[ CREATOR   ][ ${nameString}${createPadding(nameString.length, monochromeVersionString.length)} ]
+[ LIBRARY   ][ ${erisVersionString}${createPadding(erisVersionString.length, monochromeVersionString.length)} ]
+[ FRAMEWORK ][ Monochrome v${monochromeVersion} ]
 
 Need help, want to report a bug, make a suggestion, etc? Visit https://discordapp.com/invite/zkAKbyJ
 
