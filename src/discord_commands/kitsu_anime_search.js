df3ff0779489283bdfcecf2c128a966d0734ec66
Alkhwarizm <iqbal.alkhowarizmi@gmail.com>
2019-03-09 19:03:58 +0700

    Deleted unnecessary code and fixed description for kitsu command.
    Former-commit-id: b01b1a2b67646eb4a84e175c6f0491d8c92a6727

--- src/discord_commands/kitsu_anime_search.js
+++ src/discord_commands/kitsu_anime_search.js
@@ -1,7 +1,7 @@
 const reload = require('require-reload')(require);
 
 const kitsuSearch = reload('./../common/kitsu_anime_search.js');
-const { throwPublicErrorInfo, throwPublicErrorFatal } = reload('./../common/util/errors.js');
+const { throwPublicErrorInfo } = reload('./../common/util/errors.js');
 
 module.exports = {
   commandAliases: ['kitsu'],
@@ -9,12 +9,13 @@ module.exports = {
   canBeChannelRestricted: true,
   cooldown: 3,
   uniqueId: 'kitsuanime92837',
-  shortDescription: 'Search Kitsu for anime data.',
-  longDescription: 'Search Kitsu for anime data. This description should be longer, though...',
-  usageExample: '<prefix>kitsu one piece',
+  shortDescription: 'Search Kitsu.io for anime information.',
+  longDescription: 'Search Kitsu.io for anime information, including synopsis, rating and favorite counts.',
+  usageExample: '<prefix>kitsu Monster',
   action: async function action(bot, msg, suffix, monochrome) {
     if (!suffix) {
-      return throwPublicErrorInfo('Kitsu Anime', 'This feature is not yet implemented. \n--Alkh', 'WIP');
+      const { prefix } = msg;
+      return throwPublicErrorInfo('Kitsu Anime Search', `Say **${prefix}kitsu [anime]** to search for anime on Kitsu.io. For example: **${prefix}kitsu Monster**. Say **${prefix}help kitsu** for more help.`, 'No suffix');
     }
 
     return kitsuSearch.createAnimeResult(
