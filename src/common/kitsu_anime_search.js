95c94519e9f5967fb7ec6281b13765e9d29712ac
Alkhwarizm <iqbal.alkhowarizmi@gmail.com>
2019-03-10 15:16:16 +0700

    Fixed code style and cleaned up some parts in anime search.

--- src/common/kitsu_anime_search.js
+++ src/common/kitsu_anime_search.js
@@ -14,15 +14,48 @@ async function searchAnime(keyword) {
   try {
     const { data } = await api.get('anime', {
       fields: {
-        anime: 'canonicalTitle,synopsis,posterImage,averageRating,favoritesCount,slug,popularityRank,ratingRank'
+        anime: 'canonicalTitle,synopsis,posterImage,averageRating,favoritesCount,slug,popularityRank,ratingRank',
       },
       filter: { text: keyword },
-      page: { limit: 10 }
+      page: { limit: 10 },
     });
     return data;
   } catch (error) {
     return throwPublicErrorFatal('Kitsu Anime Search', 'Sorry, there is something wrong when searching Kitsu. Please try again later.', error.message);
   }
+}
+
+function formatAnimeData(animeData, callerName) {
+  const discordContent = [];
+  animeData.forEach((item) => {
+    const embed = {
+      title: item.canonicalTitle,
+      description: item.synopsis,
+      url: `${KITSU_PAGE_BASE_URI}${item.slug}`,
+      color: constants.EMBED_NEUTRAL_COLOR,
+      fields: [
+        {
+          name: 'Rating:',
+          value: `:star: **${item.averageRating}** #${item.ratingRank}`,
+          inline: true,
+        },
+        {
+          name: 'Favorites:',
+          value: `:heart: **${item.favoritesCount}** #${item.popularityRank}`,
+          inline: true,
+        },
+      ],
+      thumbnail: { url: item.posterImage.small },
+      footer: {
+        icon_url: constants.FOOTER_ICON_URI,
+        text: `${callerName} can use the reaction buttons below to see more information!`,
+      },
+    };
+
+    const trimmed = trimEmbed({ embed });
+    discordContent.push(trimmed);
+  });
+  return discordContent;
 }
 
 async function createNavigationForAnime(authorName, authorId, keyword, msg, navigationManager) {
@@ -32,40 +65,11 @@ async function createNavigationForAnime(authorName, authorId, keyword, msg, navi
     return throwPublicErrorInfo('Kitsu Anime Search', `I didn't find any results for **${keyword}**.`, 'No results');
   }
 
-  const discordContent = []
-  for (item of searchResults) {
-    const embed = {
-      title: item.canonicalTitle,
-      description: item.synopsis,
-      url: `${KITSU_PAGE_BASE_URI}${item.slug}`,
-      color: constants.EMBED_NEUTRAL_COLOR,
-      fields: [
-        { 
-          name: 'Rating:', 
-          value: `:star: **${item.averageRating}** #${item.ratingRank}`,
-          inline: true 
-        },
-        { 
-          name: 'Favorites:', 
-          value: `:heart: **${item.favoritesCount}** #${item.popularityRank}`, 
-          inline: true 
-        },
-      ],
-      thumbnail: { url: item.posterImage.small },
-      footer: {
-        icon_url: constants.FOOTER_ICON_URI,
-        text: `${authorName} can use the reaction buttons below to see more information!`
-      }
-    }
-
-    const trimmed = trimEmbed({ embed });
-    discordContent.push(trimmed);
-  }
-
+  const discordContent = formatAnimeData(searchResults, authorName);
   const navigation = Navigation.fromOneDimensionalContents(authorId, discordContent);
   return navigationManager.show(navigation, constants.NAVIGATION_EXPIRATION_TIME, msg.channel, msg);
 }
 
 module.exports = {
-  createNavigationForAnime
-}
+  createNavigationForAnime,
+};
--- src/discord_commands/kitsu_anime_search.js
+++ src/discord_commands/kitsu_anime_search.js
@@ -4,25 +4,26 @@ const kitsuSearch = reload('./../common/kitsu_anime_search.js');
 const { throwPublicErrorInfo } = reload('./../common/util/errors.js');
 
 module.exports = {
-  commandAliases: ['kitsu'],
-  aliasesForHelp: ['kitsu'],
+  commandAliases: ['anime', 'a'],
+  aliasesForHelp: ['anime', 'a'],
   canBeChannelRestricted: true,
   cooldown: 3,
   uniqueId: 'kitsuanime92837',
   shortDescription: 'Search Kitsu.io for anime information.',
   longDescription: 'Search Kitsu.io for anime information, including synopsis, rating and favorite counts.',
-  usageExample: '<prefix>kitsu Monster',
+  usageExample: '<prefix>anime Monster',
   action: async function action(bot, msg, suffix, monochrome) {
     if (!suffix) {
       const { prefix } = msg;
-      return throwPublicErrorInfo('Kitsu Anime Search', `Say **${prefix}kitsu [anime]** to search for anime on Kitsu.io. For example: **${prefix}kitsu Monster**. Say **${prefix}help kitsu** for more help.`, 'No suffix');
+      return throwPublicErrorInfo('Kitsu Anime Search', `Say **${prefix}a [anime]** to search for anime on Kitsu.io. For example: **${prefix}a Monster**. Say **${prefix}help anime** for more help.`, 'No suffix');
     }
 
     return kitsuSearch.createNavigationForAnime(
       msg.author.username,
-      msg.author.id, 
-      suffix, 
-      msg, 
-      monochrome.getNavigationManager());
+      msg.author.id,
+      suffix,
+      msg,
+      monochrome.getNavigationManager(),
+    );
   },
 };
