488223b62e34c6098cbaff0ba575f70b6ed7c8e0
Randall Schmidt <randallschmidt1@gmail.com>
2019-04-16 21:12:02 +0700

    dont register the service worker, its caching implications are causing annoyance
    Former-commit-id: fc2038782d140f51d73e9925d4c0397a6b5b21f3

--- kotobaweb/src/index.js
+++ kotobaweb/src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './main.css';
 import App from './app/App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
