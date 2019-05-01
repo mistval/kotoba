e3e31f6557a366ef79e192fdfa17fa9cead0591e
Randall Schmidt <randallschmidt1@gmail.com>
2019-05-01 21:40:27 +0700

    create cron job in backup init
    Former-commit-id: 88fd242a58860e7a3e9b819cb29bda2f6daac84b

--- backup/init.sh
+++ backup/init.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
 docker pull google/cloud-sdk
+
+echo "0 9 * * * root sh $parent_path/backup.sh" > /etc/cron.d/kotoba_backup
