3cf651d2a4ad88d01961d801fac190f4decd3859
Randall Schmidt <randallschmidt1@gmail.com>
2019-05-04 12:54:43 +0700

    change backup interval to twice per day
    Former-commit-id: 315b5eac21df3603e7bd071a93e671066f4a477d

--- backup/init.sh
+++ backup/init.sh
@@ -4,4 +4,4 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 docker pull google/cloud-sdk
 
-echo "0 9 * * * root sh $parent_path/backup.sh" > /etc/cron.d/kotoba_backup
+echo "0 9,21 * * * root sh $parent_path/backup.sh" > /etc/cron.d/kotoba_backup
