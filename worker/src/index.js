a2f0c5da8f43d57923c7d7861745c5ff6f0a9b7e
Randall Schmidt <randallschmidt1@gmail.com>
2019-07-28 00:04:03 +0700

    add a calculate stats function that runs in a worker thread. it just returns the number of game reports for now

--- worker/src/index.js
+++ worker/src/index.js
@@ -1,13 +1,35 @@
 const WorkerPool = require('./worker_pool.js');
 const path = require('path');
+const polka = require('polka');
+const send = require('@polka/send-type');
 
+const PORT = parseInt(process.env.PORT || 80);
 const WORKER_JOBS_PATH = path.join(__dirname, 'worker_jobs.js');
 const NUM_WORKERS = parseInt(process.env.NUM_WORKERS || 4);
 
-const pool = new WorkerPool(NUM_WORKERS, WORKER_JOBS_PATH);
+const workerPool = new WorkerPool(NUM_WORKERS, WORKER_JOBS_PATH);
+const cachedStatsForUser = {};
 
-pool.dispatch('say something').then((result) => {
-  console.log(result);
-}).catch((err) => {
-  console.warn(err);
+polka().get('/users/:userId/quizstats/data', async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const result = await workerPool.dispatch(
+      'calculateStats',
+      {
+        userId,
+        cachedStats: cachedStatsForUser[req.params.userId],
+      },
+    );
+
+    cachedStatsForUser[userId] = result;
+    return send(res, 200, result);
+  } catch (err) {
+    return send(res, 500, err);
+  }
+}).listen(PORT, (err) => {
+  if (err) {
+    console.warn(`Error starting`);
+    console.warn(err);
+    process.exit(1);
+  }
 });
