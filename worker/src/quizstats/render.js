2cd8cca3264aefb6a56f8ece1c8adbb72231a28f
Randall Schmidt <randallschmidt1@gmail.com>
2019-08-03 18:06:26 +0700

    render percent correct chart
    Former-commit-id: 609c24e2e23efd5a6d6b59d1e7dc94860997f29c

--- worker/src/quizstats/render.js
+++ worker/src/quizstats/render.js
@@ -178,6 +178,7 @@ const stackedBarChartSpec = {
   width: 500,
   height: 200,
   padding: 5,
+  background: '#36393f',
 
   data: [
     {
@@ -283,11 +284,155 @@ async function renderPointsPerDayChart(stats) {
   return buf;
 }
 
-function renderAggregate(stats) {
-  return Promise.all([
+// https://vega.github.io/vega/examples/line-chart/
+const percentCorrectChartSpec = {
+  $schema: 'https://vega.github.io/schema/vega/v5.json',
+  width: 500,
+  height: 200,
+  padding: 5,
+  title: {
+    text: 'Percent Correct (5 day WMA)',
+    color: '#ffffff',
+  },
+  background: '#36393f',
+
+  signals: [
+    {
+      name: 'interpolate',
+      value: 'linear',
+      bind: {
+        input: 'select',
+        options: [
+          'basis',
+          'cardinal',
+          'catmull-rom',
+          'linear',
+          'monotone',
+          'natural',
+          'step',
+          'step-after',
+          'step-before',
+        ],
+      },
+    },
+  ],
+
+  data: [
+    {
+      name: 'table',
+      values: [
+        { x: 0, y: 28, c: 0 }, { x: 0, y: 20, c: 1 },
+        { x: 1, y: 43, c: 0 }, { x: 1, y: 35, c: 1 },
+        { x: 2, y: 81, c: 0 }, { x: 2, y: 10, c: 1 },
+        { x: 3, y: 19, c: 0 }, { x: 3, y: 15, c: 1 },
+        { x: 4, y: 52, c: 0 }, { x: 4, y: 48, c: 1 },
+        { x: 5, y: 24, c: 0 }, { x: 5, y: 28, c: 1 },
+        { x: 6, y: 87, c: 0 }, { x: 6, y: 66, c: 1 },
+        { x: 7, y: 17, c: 0 }, { x: 7, y: 27, c: 1 },
+        { x: 8, y: 68, c: 0 }, { x: 8, y: 16, c: 1 },
+        { x: 9, y: 49, c: 0 }, { x: 9, y: 25, c: 1 },
+      ],
+    },
+  ],
+
+  scales: [
+    {
+      name: 'x',
+      type: 'point',
+      range: 'width',
+      domain: { data: 'table', field: 'x' },
+    },
+    {
+      name: 'y',
+      type: 'linear',
+      range: 'height',
+      nice: true,
+      zero: true,
+      domain: { data: 'table', field: 'y' },
+    },
+    {
+      name: 'color',
+      type: 'ordinal',
+      range: 'category',
+      domain: { data: 'table', field: 'c' },
+    },
+  ],
+
+  axes: [
+    {
+      orient: 'bottom', scale: 'x', labelColor: '#ffffff', title: 'Days ago', titleColor: '#ffffff',
+    },
+    {
+      orient: 'left', scale: 'y', labelColor: '#ffffff', title: 'Percent Correct', titleColor: '#ffffff',
+    },
+  ],
+
+  marks: [
+    {
+      type: 'group',
+      from: {
+        facet: {
+          name: 'series',
+          data: 'table',
+          groupby: 'c',
+        },
+      },
+      marks: [
+        {
+          type: 'line',
+          from: { data: 'series' },
+          encode: {
+            enter: {
+              x: { scale: 'x', field: 'x' },
+              y: { scale: 'y', field: 'y' },
+              stroke: { scale: 'color', field: 'c' },
+              strokeWidth: { value: 2 },
+            },
+            update: {
+              interpolate: { signal: 'interpolate' },
+              fillOpacity: { value: 1 },
+            },
+            hover: {
+              fillOpacity: { value: 0.5 },
+            },
+          },
+        },
+      ],
+    },
+  ],
+};
+
+async function renderPercentCorrectWMAChart(stats) {
+  const spec = { ...percentCorrectChartSpec };
+  spec.width = stats.percentCorrectWMA.length * 18;
+  spec.data = [...spec.data];
+  spec.data[0] = { ...spec.data[0] };
+  spec.data[0].values = stats.percentCorrectWMA.map((wma, i) => {
+    const daysAgo = stats.dailyStats.length - i;
+    return {
+      x: daysAgo,
+      y: wma,
+      c: 0,
+    };
+  });
+
+  const buf = await renderSpecToBuffer(spec);
+
+  return buf;
+}
+
+async function renderAggregate(stats) {
+  const charts = await Promise.all([
     renderDeckPieChart(stats),
     renderPointsPerDayChart(stats),
+    renderPercentCorrectWMAChart(stats),
   ]);
+
+  return {
+    deckPieChart: charts[0].toString('base64'),
+    pointsPerDayChart: charts[1].toString('base64'),
+    percentCorrectChart: charts[2].toString('base64'),
+  };
 }
 
 module.exports = renderAggregate;
