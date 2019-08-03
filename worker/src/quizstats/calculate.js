12593be7b1524b3fce69ae29a39a5ff87480abbf
Randall Schmidt <randallschmidt1@gmail.com>
2019-08-03 19:00:33 +0700

    have it accept discord user IDs
    Former-commit-id: 60b754dd7b9f4d16433d46a938e5abeb4675710d

--- worker/src/quizstats/calculate.js
+++ worker/src/quizstats/calculate.js
@@ -1,15 +1,36 @@
 const dbConnection = require('kotoba-node-common').database.connection;
 const GameReportModel = require('kotoba-node-common').models.createGameReportModel(dbConnection);
+const UserModel = require('kotoba-node-common').models.createUserModel(dbConnection);
 const render = require('./render.js');
+const mongoose = require('mongoose');
 
 const cachedStatsForUserId = {};
 
 async function calculateStats(workerPool, userId) {
-  const cachedStats = cachedStatsForUserId[userId];
+  // Find the user's ID in mongo, if a Discord user ID was given.
+  // If no ID can be resolved, return undefined.
+  let mongoUserId;
+  try {
+    mongoUserId = mongoose.Types.ObjectId(userId);
+  } catch (err) {
+    const userRecord = await UserModel.findOne({ 'discordUser.id': userId })
+      .select('_id')
+      .lean()
+      .exec();
+
+    if (userRecord) {
+      mongoUserId = userRecord._id;
+    } else {
+      return undefined;
+    }
+  }
+
+  const mongoUserIdStr = mongoUserId.toString();
+  const cachedStats = cachedStatsForUserId[mongoUserIdStr];
 
   if (cachedStats) {
     const mostRecentReport = await GameReportModel
-      .find({ participants: userId })
+      .find({ participants: mongoUserId })
       .sort({ startTime: -1 })
       .limit(1)
       .select('_id')
@@ -24,17 +45,16 @@ async function calculateStats(workerPool, userId) {
     }
   }
 
-  console.time('Calculate quiz stats');
-
-  const stats = await workerPool.doWork('calculateStats', userId);
+  const stats = await workerPool.doWork('calculateStats', mongoUserIdStr);
+  if (!stats) {
+    return undefined;
+  }
 
   // TODO: Canvas doesn't support worker threads, RIP
   // https://github.com/Automattic/node-canvas/issues/1394
   stats.charts = await render(stats);
 
-  cachedStatsForUserId[userId] = stats;
-
-  console.timeEnd('Calculate quiz stats');
+  cachedStatsForUserId[mongoUserIdStr] = stats;
 
   return stats;
 }
