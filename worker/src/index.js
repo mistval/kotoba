922e82db3a9eccfc8fafbcfe24c0c0610f568fcf
Randall Schmidt <randallschmidt1@gmail.com>
2019-08-10 15:24:05 +0700

    make it 404 if no stats available

--- worker/src/index.js
+++ worker/src/index.js
@@ -10,10 +10,12 @@ const WORKER_JOBS_PATH = path.join(__dirname, 'worker_jobs.js');
 const workerPool = new WorkerPool(WORKER_JOBS_PATH);
 
 polka().get('/users/:userId/quizstats', async (req, res) => {
-  // try {
   const result = await calculateStats(workerPool, req.params.userId);
-  return send(res, 200, result);
-  // }
+  if (result) {
+    return send(res, 200, result);
+  } else {
+    return send(res, 404);
+  }
 }).listen(PORT, (err) => {
   if (err) {
     console.warn('Error starting');
