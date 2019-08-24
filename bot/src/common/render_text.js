9aa705f2ab811b5c0d09bc6f8f83962037a66c7e
Randall Schmidt <randallschmidt1@gmail.com>
2019-08-24 13:07:07 +0700

    update to canvas 2.6.0

--- bot/src/common/render_text.js
+++ bot/src/common/render_text.js
@@ -13,40 +13,35 @@ const TOTAL_VERTICAL_PADDING_IN_PIXELS = TOP_PADDING_IN_PIXELS + BOTTOM_PADDING_
 
 function render(text, textColor = 'black', backgroundColor = 'white', fontSize = 96, fontSetting = 'Yu Mincho') {
   const font = fontHelper.getFontNameForFontSetting(fontSetting);
-  return new Promise((fulfill, reject) => {
-    const canvas = new Canvas(0, 0);
-    const ctx = canvas.getContext('2d');
-    ctx.font = `${fontSize}px ${font}`;
+  const canvas = Canvas.createCanvas(0, 0);
+  const ctx = canvas.getContext('2d');
+  ctx.font = `${fontSize}px ${font}`;
 
-    const measurements = ctx.measureText(text);
+  const measurements = ctx.measureText(text);
 
-    const leftPaddingInPixels = BASE_LEFT_PADDING_IN_PIXELS * Math.floor((text.length / 4) + 1);
-    const rightPaddingInPixels = BASE_RIGHT_PADDING_IN_PIXELS * Math.floor((text.length / 4) + 1);
-    const totalHorizontalPaddingInPixels = leftPaddingInPixels + rightPaddingInPixels;
+  const leftPaddingInPixels = BASE_LEFT_PADDING_IN_PIXELS * Math.floor((text.length / 4) + 1);
+  const rightPaddingInPixels = BASE_RIGHT_PADDING_IN_PIXELS * Math.floor((text.length / 4) + 1);
+  const totalHorizontalPaddingInPixels = leftPaddingInPixels + rightPaddingInPixels;
 
-    canvas.width = measurements.width + totalHorizontalPaddingInPixels;
-    canvas.height = measurements.actualBoundingBoxAscent +
-      measurements.actualBoundingBoxDescent +
-      TOTAL_VERTICAL_PADDING_IN_PIXELS;
+  canvas.width = measurements.width + totalHorizontalPaddingInPixels;
+  canvas.height = measurements.actualBoundingBoxAscent +
+    measurements.actualBoundingBoxDescent +
+    TOTAL_VERTICAL_PADDING_IN_PIXELS;
 
-    ctx.fillStyle = backgroundColor;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = backgroundColor;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = textColor;
-    ctx.fillText(
-      text,
-      leftPaddingInPixels,
-      measurements.actualBoundingBoxAscent + TOP_PADDING_IN_PIXELS,
-    );
+  ctx.fillStyle = textColor;
+  ctx.font = `${fontSize}px ${font}`;
+  ctx.fillText(
+    text,
+    leftPaddingInPixels,
+    measurements.actualBoundingBoxAscent + TOP_PADDING_IN_PIXELS,
+  );
 
-    canvas.toBuffer((err, buffer) => {
-      if (err) {
-        reject(err);
-      } else {
-        fulfill(buffer);
-      }
-    }, 9, canvas.PNG_FILTER_NONE);
-  });
+  const bufferOptions = { compressionLevel: 9, filters: canvas.PNG_FILTER_NONE };
+  const buffer = canvas.toBuffer('image/png', bufferOptions);
+  return buffer;
 }
 
 function renderJapaneseWithFurigana(text, mainFontSize, textColor, backgroundColor, fontSetting) {
