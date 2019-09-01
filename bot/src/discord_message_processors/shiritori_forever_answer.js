92a0f2442b2e7019cdd6139ffe933b5e71f387cc
Randall Schmidt <randallschmidt1@gmail.com>
2019-09-01 19:28:41 +0700

    demote quiz and shiritori answers to debug log level

--- bot/src/discord_message_processors/shiritori_forever_answer.js
+++ bot/src/discord_message_processors/shiritori_forever_answer.js
@@ -4,5 +4,6 @@ const shiritoriForeverHelper = require('./../discord/shiritori_forever_helper');
 
 module.exports = {
   name: 'Shiritori Forever Answer',
+  logLevel: 'debug',
   action: (bot, msg, monochrome) => shiritoriForeverHelper.tryHandleMessage(monochrome, msg),
 };
