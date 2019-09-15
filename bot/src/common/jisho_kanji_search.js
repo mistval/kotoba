fb9f0d17e4a4e92986d3c721cb79363cf7fcd473
Alkhwarizm <iqbal.alkhowarizmi@gmail.com>
2019-09-15 13:47:50 +0700

    Cleaning up and lint fixing.

--- bot/src/common/jisho_kanji_search.js
+++ bot/src/common/jisho_kanji_search.js
@@ -8,16 +8,16 @@ const extractKanji = require('./util/extract_kanji.js');
 const jishoApi = new UnofficialJishoApi();
 
 async function searchKanjiFromWord(word) {
-    const kanjiURI = jishoApi.getUriForKanjiSearch(word);
-    
-    try {
-        const response = await axios.get(kanjiURI);
-        const $ = cheerio.load(response.data);
-        const kanjis = $('.character a').text();
-        return extractKanji(kanjis);
-    } catch (err) {
-        return errors.throwPublicErrorFatal('Jisho', 'Sorry, Jisho is not responding. Please try again later.', 'Error fetching from Jisho', err);
-    }
+  const kanjiURI = jishoApi.getUriForKanjiSearch(word);
+
+  try {
+    const response = await axios.get(kanjiURI);
+    const $ = cheerio.load(response.data);
+    const kanjis = $('.character a').text();
+    return extractKanji(kanjis);
+  } catch (err) {
+    return errors.throwPublicErrorFatal('Jisho', 'Sorry, Jisho is not responding. Please try again later.', 'Error fetching from Jisho', err);
+  }
 }
 
 module.exports = searchKanjiFromWord;
--- bot/src/discord/create_kanji_search_navigation_chapter.js
+++ bot/src/discord/create_kanji_search_navigation_chapter.js
@@ -3,7 +3,6 @@ const extractKanji = require('../common/util/extract_kanji.js');
 const createKanjiSearchPage = require('./create_kanji_search_page.js');
 const addPaginationFooter = require('./add_pagination_footer.js');
 const jishoKanjiSearch = require('./../common/jisho_kanji_search.js');
-const constants = require('./../common/constants.js');
 
 class KanjiNavigationDataSource {
   constructor(kanjis, authorName, commandPrefix, forceNavigationFooter) {
@@ -50,19 +49,6 @@ async function createKanjiSearchNavigationChapter(
 ) {
   let kanji = extractKanji(searchQuery);
   if (kanji.length === 0) {
-    // const pages = [{
-    //   embed: {
-    //     title: 'Jisho Kanji Search',
-    //     description: `I didn't find any kanji in your search query: ${searchQuery}. Try searching for some kanji.`,
-    //     color: constants.EMBED_NEUTRAL_COLOR,
-    //   },
-    // }];
-
-    // return {
-    //   navigationChapter: NavigationChapter.fromContent(pages),
-    //   pageCount: 1,
-    //   hasResult: false,
-    // };
     kanji = await jishoKanjiSearch(searchQuery);
   }
 
