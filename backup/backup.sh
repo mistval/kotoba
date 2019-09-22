68f63ceee07d17fc230f55826a1d59d5775c96a6
Randall Schmidt <randallschmidt1@gmail.com>
2019-09-22 13:13:40 -0400

    have backup script backup without date in filename (using versioning now)

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 bucket_name=kotoba_backup_n
+file_name=backup.tar.gz
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd $parent_path/..
@@ -14,12 +15,8 @@ cp -r ./user_data/bot/monochrome-persistence ./backup_out/user_data/bot
 cp -r ./user_data/bot/quiz_saves ./backup_out/user_data/bot
 cp -r ./user_data/shared_data ./backup_out/user_data
 
-now=$(date +"%m-%d-%Y-%H-%M")
-object_name="backup_$now"
-file_name="$object_name.tar.gz"
-
 rm -rf ./gsutil_volume
 mkdir ./gsutil_volume
 tar cvfz ./gsutil_volume/$file_name ./backup_out
 
-docker run -v $parent_path/../gsutil_volume/:/var/backup -v $parent_path/../backup:/var/gcloud_key google/cloud-sdk /bin/bash -c "gcloud auth activate-service-account --key-file=/var/gcloud_key/gcloud_key.json && gsutil cp /var/backup/* gs://$bucket_name"
+docker run -v $parent_path/../gsutil_volume/:/var/backup -v $parent_path/../backup:/var/gcloud_key google/cloud-sdk:alpine /bin/bash -c "gcloud auth activate-service-account --key-file=/var/gcloud_key/gcloud_key.json && gsutil cp /var/b$
