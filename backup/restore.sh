103a36dcfd7fdd1bcb73d0bfe87beec628e9356f
Randall Schmidt <randallschmidt1@gmail.com>
2019-09-22 13:34:07 -0400

    have restore script pull down the latest backup from cloud storage
    Former-commit-id: 7be3c3c9d21651e592b9a334c0ec3267d65c0949

--- backup/restore.sh
+++ backup/restore.sh
@@ -1,13 +1,16 @@
+#!/bin/bash
+
+bucket_name=kotoba_backup_n
+file_name=backup.tar.gz
+
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd $parent_path
 
-compressed_backup=$parent_path/$(ls backup_*.tar.gz)
-
-echo Restoring from $compressed_backup
+docker run -v $parent_path:/var/backup google/cloud-sdk:alpine /bin/bash -c "gcloud auth activate-service-account --key-file=/var/backup/gcloud_key.json && gsutil cp gs://$bucket_name/$file_name /var/backup"
 
 rm -rf ./temp
 mkdir temp
-tar xzf $compressed_backup -C ./temp
+tar xzf $file_name -C ./temp
 
 cd ..
 
@@ -20,4 +23,4 @@ rm -rf ./user_data/shared_data/custom_decks
 mv -f ./backup/temp/backup_out/user_data/bot/* ./user_data/bot/
 mv -f ./backup/temp/backup_out/user_data/shared_data/* ./user_data/shared_data/
 
-rm -rf ./temp
+rm -rf ./backup/temp
