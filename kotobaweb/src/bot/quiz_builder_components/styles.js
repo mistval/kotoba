4b2ee00975266ae1171fa709f3bfbedf6dc2bfb0
Randall Schmidt <randallschmidt1@gmail.com>
2020-01-01 20:49:21 -0500

    refactor the timing editor into its own file

--- kotobaweb/src/bot/quiz_builder_components/styles.js
+++ kotobaweb/src/bot/quiz_builder_components/styles.js
@@ -2,4 +2,10 @@ module.exports = {
   card: {
     minHeight: '400px',
   },
+  timingModal: {
+    maxWidth: '575px',
+  },
+  commandText: {
+    fontSize: '28px',
+  },
 };
--- kotobaweb/src/bot/quiz_builder_components/util.js
+++ kotobaweb/src/bot/quiz_builder_components/util.js
@@ -1,4 +1,6 @@
-function convertRangeNumberToString(number) {
+import { quizTimeModifierPresets, quizDefaults } from 'kotoba-common';
+
+export function convertRangeNumberToString(number) {
   if (number === Number.POSITIVE_INFINITY) {
     return 'end';
   }
@@ -6,7 +8,7 @@ function convertRangeNumberToString(number) {
   return number.toString();
 }
 
-function convertRangeStringToNumber(string) {
+export function convertRangeStringToNumber(string) {
   if (string === 'end') {
     return Number.POSITIVE_INFINITY;
   }
@@ -14,12 +16,42 @@ function convertRangeStringToNumber(string) {
   return parseInt(string);
 }
 
-function createDeck(name, startIndex = 1, endIndex = Number.POSITIVE_INFINITY) {
+export function createDeck(name, startIndex = 1, endIndex = Number.POSITIVE_INFINITY) {
   return { name, startIndex, endIndex };
 }
 
-module.exports = {
-  convertRangeNumberToString,
-  convertRangeStringToNumber,
-  createDeck,
-};
+export function createTimeModifierParts(timingArguments) {
+  const presetEntries = Object.entries(quizTimeModifierPresets);
+  for (let i = 0; i < presetEntries.length; i += 1) {
+    const presetEntry = presetEntries[i];
+    const [presetName, presetValues] = presetEntry;
+
+    if (
+      presetValues.answerTimeLimit === timingArguments.answerTimeLimit
+      && presetValues.delayAfterUnansweredQuestion === timingArguments.delayAfterUnansweredQuestion
+      && presetValues.delayAfterAnsweredQuestion === timingArguments.delayAfterAnsweredQuestion
+      && presetValues.additionalAnswerWaitWindow === timingArguments.additionalAnswerWaitWindow) {
+        if (presetName === 'normal') {
+          return [];
+        }
+        return [presetName];
+      }
+  }
+
+  const parts = [];
+
+  if (quizDefaults.answerTimeLimit !== timingArguments.answerTimeLimit) {
+    parts.push(`answerTimeLimit=${timingArguments.answerTimeLimit}`);
+  }
+  if (quizDefaults.delayAfterUnansweredQuestion !== timingArguments.delayAfterUnansweredQuestion) {
+    parts.push(`delayAfterUnansweredQuestion=${timingArguments.delayAfterUnansweredQuestion}`);
+  }
+  if (quizDefaults.delayAfterAnsweredQuestion !== timingArguments.delayAfterAnsweredQuestion) {
+    parts.push(`delayAfterAnsweredQuestion=${timingArguments.delayAfterAnsweredQuestion}`);
+  }
+  if (quizDefaults.additionalAnswerWaitWindow !== timingArguments.additionalAnswerWaitWindow) {
+    parts.push(`additionalAnswerWaitWindow=${timingArguments.additionalAnswerWaitWindow}`);
+  }
+
+  return parts;
+}
