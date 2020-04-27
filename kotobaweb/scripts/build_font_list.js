9a5b78314f82557d60564dcdbcde8e3749036c96
Randall Schmidt <randallschmidt1@gmail.com>
2020-04-27 18:36:35 -0400

    add automatically generated font list

---
+++ kotobaweb/scripts/build_font_list.js
@@ -0,0 +1,14 @@
+const glob = require('glob');
+const fs = require('fs');
+const path = require('path');
+
+const baseFontDirPath = path.join(__dirname, '..', '..', 'resources', 'fonts');
+const fontMetaPaths = glob.sync(path.join(baseFontDirPath, '**', '*.json'));
+const fontList = fontMetaPaths
+  .map(p => JSON.parse(fs.readFileSync(p)))
+  .filter(meta => !meta.hidden)
+  .sort((a, b) => a.order - b.order)
+  .map(meta => meta.fontFamily);
+
+const fontListPath = path.join(__dirname, '..', 'src', 'bot', 'quiz_builder_components', 'font_list.json');
+fs.writeFileSync(fontListPath, JSON.stringify(fontList));
