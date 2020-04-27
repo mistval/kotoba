67c9ff38a09b9e2ef08fd983a7148c8a845ae5a0
Randall Schmidt <randallschmidt1@gmail.com>
2020-04-27 18:47:35 -0400

    add font size control

--- kotobaweb/src/bot/quiz_builder_components/font_editor.jsx
+++ kotobaweb/src/bot/quiz_builder_components/font_editor.jsx
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import { SketchPicker } from 'react-color';
 import styles from './styles';
 import fontList from './font_list.json';
+import NumericInputBox from './../../controls/numeric_input_box';
 
 class FontEditor extends PureComponent {
   handleTextColorChanged = (textColor) => {
@@ -25,6 +26,13 @@ class FontEditor extends PureComponent {
     });
   }
 
+  handleFontSizeChanged = (fontSize) => {
+    this.props.onFontSettingsChanged({
+      ...this.props.fontSettings,
+      fontSize,
+    });
+  }
+
   render() {
     return (
       <div className="card" style={styles.card}>
@@ -42,13 +50,21 @@ class FontEditor extends PureComponent {
           </div>
           <div>
             <div className="form-group">
-              <label htmlFor="exampleFormControlSelect1">Font Family</label>
-              <select className="form-control" id="exampleFormControlSelect1" onChange={this.handleFontFamilyChanged} >
+              <label htmlFor="fontFamilySelect">Font Family</label>
+              <select className="form-control" id="fontFamilySelect" onChange={this.handleFontFamilyChanged} >
                 { fontList.map((f, i) => 
                   <option key={i} selected={i === this.props.fontSettings.fontFamilyIndex ? 'selected' : ''}>{f}</option>
                 ) }
               </select>
             </div>
+            <label>Font Size</label>
+            <NumericInputBox
+              value={this.props.fontSettings.fontSize}
+              minValue={20}
+              maxValue={200}
+              onChange={this.handleFontSizeChanged}
+              maxPlacesAfterDecimal={0}
+            />
           </div>
         </div>
       </div>
