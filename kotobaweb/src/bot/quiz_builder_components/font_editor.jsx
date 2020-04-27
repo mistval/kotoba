e7b8fc0ad966a482817d60b034a44bac85cb97d7
Randall Schmidt <randallschmidt1@gmail.com>
2020-04-27 16:44:28 -0400

    make fonteditor interactive and wire up to parent state

--- kotobaweb/src/bot/quiz_builder_components/font_editor.jsx
+++ kotobaweb/src/bot/quiz_builder_components/font_editor.jsx
@@ -1,19 +1,41 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
+import { SketchPicker } from 'react-color';
 import styles from './styles';
 
-class DeckEditor extends Component {
+class FontEditor extends PureComponent {
+  handleTextColorChanged = (textColor) => {
+    this.props.onFontSettingsChanged({
+      ...this.props.fontSettings,
+      textColor: textColor.hex,
+    });
+  }
+
+  handleBackgroundColorChanged = (backgroundColor) => {
+    this.props.onFontSettingsChanged({
+      ...this.props.fontSettings,
+      backgroundColor: backgroundColor.hex,
+    });
+  }
+
   render() {
     return (
       <div className="card" style={styles.card}>
         <div className="card-block-title">
           <h5 className="card-title d-inline-block">Font settings</h5>
         </div>
-        <div className="card-body">
-          
+        <div className="card-body d-flex flex-row">
+          <div className="mr-5">
+            <h6 class="text-center">Text Color</h6>
+            <SketchPicker color={this.props.fontSettings.textColor} onChange={this.handleTextColorChanged} />
+          </div>
+          <div>
+            <h6 class="text-center">Background Color</h6>
+            <SketchPicker color={this.props.fontSettings.backgroundColor} onChange={this.handleBackgroundColorChanged} />
+          </div>
         </div>
       </div>
     );
   }
 }
 
-export default DeckEditor;
+export default FontEditor;
