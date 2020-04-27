df021571554e97e2abcd34411a82c6c16bd106e6
Randall Schmidt <randallschmidt1@gmail.com>
2020-04-27 17:03:16 -0400

    add some boilerplate for the font family selector

--- kotobaweb/src/bot/quiz_builder_components/font_editor.jsx
+++ kotobaweb/src/bot/quiz_builder_components/font_editor.jsx
@@ -17,6 +17,13 @@ class FontEditor extends PureComponent {
     });
   }
 
+  handleFontFamilyChanged = (ev) => {
+    this.props.onFontSettingsChanged({
+      ...this.props.fontSettings,
+      fontFamilyIndex: ev.target.selectedIndex,
+    });
+  }
+
   render() {
     return (
       <div className="card" style={styles.card}>
@@ -25,12 +32,24 @@ class FontEditor extends PureComponent {
         </div>
         <div className="card-body d-flex flex-row">
           <div className="mr-5">
-            <h6 class="text-center">Text Color</h6>
+            <h6 className="text-center">Text Color</h6>
             <SketchPicker color={this.props.fontSettings.textColor} onChange={this.handleTextColorChanged} />
           </div>
-          <div>
-            <h6 class="text-center">Background Color</h6>
+          <div className="mr-5">
+            <h6 className="text-center">Background Color</h6>
             <SketchPicker color={this.props.fontSettings.backgroundColor} onChange={this.handleBackgroundColorChanged} />
+          </div>
+          <div>
+            <div className="form-group">
+              <label htmlFor="exampleFormControlSelect1">Font Family</label>
+              <select className="form-control" id="exampleFormControlSelect1" onChange={this.handleFontFamilyChanged} >
+                <option>1ggggggggggggggggggggggggggggggggggggggggggggg</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </select>
+            </div>
           </div>
         </div>
       </div>
