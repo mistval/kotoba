500f0727947438f0039e6316bf7ccca3efb6144e
Randall Schmidt <randallschmidt1@gmail.com>
2020-04-29 08:56:07 -0400

    add a note about font families and scaling

--- kotobaweb/scripts/build_font_list.js
+++ kotobaweb/scripts/build_font_list.js
@@ -8,7 +8,7 @@ const fontList = fontMetaPaths
   .map(p => JSON.parse(fs.readFileSync(p)))
   .filter(meta => !meta.hidden)
   .sort((a, b) => a.order - b.order)
-  .map(meta => meta.fontFamily);
+  .map(meta => [meta.fontFamily, ...meta.cssFontNames]);
 
 const fontListPath = path.join(__dirname, '..', 'src', 'bot', 'quiz_builder_components', 'font_list.json');
 fs.writeFileSync(fontListPath, JSON.stringify(fontList));
