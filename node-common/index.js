043047c45161b330f087ba22f8e796d475075659
Randall Schmidt <randallschmidt1@gmail.com>
2020-05-15 16:51:52 -0400

    use the resource database for the pronounce command
    Former-commit-id: 46d25b8fd8f26c9de68eb38103e5b1e9d3e17cd0

--- node-common/index.js
+++ node-common/index.js
@@ -2,8 +2,10 @@ const models = require('./mongoose_models');
 const database = require('./mongodb.js');
 const constants = require('./constants.js');
 const initializeFonts = require('./initialize_fonts.js');
+const initializeResourceDatabase = require('./initialize_resource_database.js');
 
 module.exports = {
+  initializeResourceDatabase,
   initializeFonts,
   models,
   database,
