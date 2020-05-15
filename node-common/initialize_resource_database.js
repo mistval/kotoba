6b6f74b087f5648cc464e15c69a671cd24d3a4a8
Randall Schmidt <randallschmidt1@gmail.com>
2020-05-15 17:42:35 -0400

    add random words to resource db
    Former-commit-id: 3f295bd8f5af58dd792e604989178cdd7d8d5da9

--- node-common/initialize_resource_database.js
+++ node-common/initialize_resource_database.js
@@ -1,13 +1,30 @@
+const fs = require('fs');
 const ResourceDatabase = require('./resource_database.js');
 
-async function initializeResourceDatabase(databasePath, pronunciationDataPath) {
+async function initializeResourceDatabase(
+  databasePath,
+  pronunciationDataPath,
+  randomWordDataPath,
+) {
   const resourceDatabase = new ResourceDatabase();
-  await resourceDatabase.load(databasePath, pronunciationDataPath);
+  await resourceDatabase.load(
+    databasePath,
+    pronunciationDataPath,
+    randomWordDataPath,
+  );
+
   return resourceDatabase;
 }
 
 if (require.main === module) {
-  initializeResourceDatabase(process.argv[2], process.argv[3]);
+  initializeResourceDatabase(
+    process.argv[2],
+    process.argv[3],
+    process.argv[4],
+  ).catch((err) => {
+    console.warn(err);
+    process.exit(1);
+  });
 }
 
 module.exports = initializeResourceDatabase;
