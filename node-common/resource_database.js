3f295bd8f5af58dd792e604989178cdd7d8d5da9
Randall Schmidt <randallschmidt1@gmail.com>
2020-05-15 17:42:35 -0400

    add random words to resource db

--- node-common/resource_database.js
+++ node-common/resource_database.js
@@ -21,8 +21,30 @@ async function buildPronunciationTable(database, pronunciationDataPath) {
   await database.run('COMMIT');
 }
 
+async function buildRandomWordsTable(database, randomWordDataPath) {
+  const randomWordData = JSON.parse(fs.readFileSync(randomWordDataPath));
+  const levels = Object.keys(randomWordData);
+
+  await database.run('CREATE TABLE RandomWords (id INTEGER PRIMARY KEY AUTOINCREMENT, level CHAR(5), word CHAR (10))');
+  const statement = await database.prepare('INSERT INTO RandomWords (level, word) VALUES (?, ?)');
+
+  await database.run('BEGIN');
+
+  for (let i = 0; i < levels.length; i += 1) {
+    const level = levels[i];
+    const words = randomWordData[level];
+
+    for (let j = 0; j < words.length; j += 1) {
+      await statement.run(level, words[j]);
+    }
+  }
+
+  await database.run('COMMIT');
+  await database.run('CREATE INDEX level ON RandomWords (level)');
+}
+
 class ResourceDatabase {
-  async load(databasePath, pronunciationDataPath) {
+  async load(databasePath, pronunciationDataPath, randomWordDataPath) {
     const needsBuild = !fs.existsSync(databasePath);
     this.database = await sqlite.open({
       filename: databasePath,
@@ -30,9 +52,8 @@ class ResourceDatabase {
     });
 
     if (needsBuild) {
-      await Promise.all([
-        buildPronunciationTable(this.database, pronunciationDataPath),
-      ]);
+      await buildPronunciationTable(this.database, pronunciationDataPath);
+      await buildRandomWordsTable(this.database, randomWordDataPath);
     }
 
     this.searchPronunciationStatement = await this.database.prepare('SELECT resultsJson FROM PronunciationSearchResults WHERE searchTerm = ?');
@@ -40,8 +61,19 @@ class ResourceDatabase {
 
   async searchPronunciation(searchTerm) {
     const result = await this.searchPronunciationStatement.get(searchTerm);
-    await this.searchPronunciationStatement.reset();
     return JSON.parse((result || {}).resultsJson || '[]');
+  }
+
+  async getRandomWord(level) {
+    if (level) {
+      const levelResult = await this.database.get('SELECT word FROM RandomWords WHERE level = ? LIMIT (ABS(RANDOM()) % (SELECT COUNT(*) FROM RandomWords WHERE level = ?)),1', level, level);
+      if (levelResult) {
+        return levelResult.word;
+      }
+    }
+
+    const result = await this.database.get('SELECT word FROM RandomWords WHERE id = ABS(RANDOM()) %(SELECT COUNT(*) FROM RandomWords)');
+    return result.word;
   }
 }
 
