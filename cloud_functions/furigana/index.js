57d9ad2152e0fe58735c648ed9a272776848aa81
Randall Schmidt <randallschmidt1@gmail.com>
2020-06-06 13:00:01 -0400

    improve line breaking and max-width for furigana rendering
    Former-commit-id: 3768521092f848981d3490ba0d02d46cca86df83

--- cloud_functions/furigana/index.js
+++ cloud_functions/furigana/index.js
@@ -4,10 +4,54 @@ const KuroshiroKuromoji = require('kuroshiro-analyzer-kuromoji');
 const kuroshiro = new Kuroshiro();
 const initPromise = kuroshiro.init(new KuroshiroKuromoji());
 
+function isBreakableCharacter(char) {
+  if (char >= '\u3040' && char <= '\u309f') {
+    return true; // hiragana
+  }
+  if (char >= '\u30a0' && char <= '\u30ff') {
+    return true; // katakana
+  }
+  if (char >= '\u4e00' && char <= '\u9faf') {
+    return true; // ideographs
+  }
+  return false;
+}
+
+function splitResults(results) {
+  const newResults = [];
+
+  results.forEach((chunk) => {
+    if (chunk.reading) {
+      newResults.push(chunk);
+    } else {
+      const characters = chunk.text.split('').reverse();
+      let nextChunkText = '';
+      while (characters.length > 0) {
+        const character = characters.pop();
+        if (isBreakableCharacter(character)) {
+          if (nextChunkText) {
+            newResults.push({ text: nextChunkText });
+            nextChunkText = '';
+          }
+          newResults.push({ text: character });
+        } else {
+          nextChunkText += character;
+        }
+      }
+
+      if (nextChunkText) {
+        newResults.push({ text: nextChunkText });
+      }
+    }
+  });
+
+  return newResults;
+}
+
 async function getFurigana(text) {
   await initPromise;
-  const result = await kuroshiro.convert(text, { to: 'hiragana', mode: 'raw' });
-  return result;
+  const results = await kuroshiro.convert(text, { to: 'hiragana', mode: 'raw' });
+  return splitResults(results);
 }
 
 /**
@@ -18,16 +62,16 @@ async function getFurigana(text) {
  */
 exports.getFurigana = async (req, res) => {
   try {
-    if (typeof req.query.text !== 'string') {
+    const text = req.body.text;
+    if (typeof text !== 'string') {
       throw new Error(`Input is not a string`);
     }
 
-    const text = decodeURIComponent(req.query.text);
     if (text.length > 500) {
       throw new Error('Input is too long');
     }
 
-    res.json(await getFurigana(req.query.text));
+    res.json(await getFurigana(text));
   } catch (err) {
     res.status(500).send(err.message);
     throw err;
