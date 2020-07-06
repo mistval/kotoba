d5a7e0f8937f7768faf0f668b73bbfa268890eb8
Randall Schmidt <randallschmidt1@gmail.com>
2020-07-06 18:08:51 -0400

    add readMessages permission to invite links

--- bot/src/discord_commands/invite.js
+++ bot/src/discord_commands/invite.js
@@ -8,6 +8,6 @@ module.exports = {
   shortDescription: 'Get a link to invite me to your server.',
   requiredBotPermissions: [Permissions.sendMessages],
   action(bot, msg) {
-    return msg.channel.createMessage(`You can use this link to invite me to your server! <https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=51264>`, null, msg);
+    return msg.channel.createMessage(`You can use this link to invite me to your server! <https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=52288>`, null, msg);
   },
 };
