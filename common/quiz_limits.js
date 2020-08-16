84348aec76381c21fc4a622b96225d2c5075c1a9
Randall Schmidt <randallschmidt1@gmail.com>
2020-08-15 23:07:11 -0400

    reduce min timeout to 3 seconds per user request

--- common/quiz_limits.js
+++ common/quiz_limits.js
@@ -1,5 +1,5 @@
 module.exports = {
-  answerTimeLimit: [5, 120],
+  answerTimeLimit: [3, 120],
   scoreLimit: [1, 10000],
   delayAfterUnansweredQuestion: [0, 120],
   delayAfterAnsweredQuestion: [0, 120],
