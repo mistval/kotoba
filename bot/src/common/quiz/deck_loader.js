baf76bf7a83d302122215ee05a651efefd11f2a5
Randall Schmidt <randallschmidt1@gmail.com>
2020-09-05 16:02:08 -0400

    fix dumbness
    Former-commit-id: 7da11e166c61397cec8abdd0846f17985210221c

--- bot/src/common/quiz/deck_loader.js
+++ bot/src/common/quiz/deck_loader.js
@@ -179,7 +179,7 @@ async function getCustomDeckFromDisk(deckInfo) {
     uniqueId: deckRaw.uniqueId,
     name: deckRaw.name,
     shortName: deckRaw.shortName,
-    description: `Custom quiz by ${deckRaw.ownerDiscordUser.username}#${deckRaw.ownerDiscordUser.discriminator}. ${deckRaw.description}`,
+    description: `Custom quiz by ${deckRaw.ownerDiscordUser.username}#${deckRaw.ownerDiscordUser.discriminator}. ${deckRaw.description || ''}`,
     article: 'a',
     dictionaryLinkStrategy: 'NONE',
     answerTimeLimitStrategy: 'JAPANESE_SETTINGS',
