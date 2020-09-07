7e09f0f1c1876048635159a006d0a08311b5c5ed
Randall Schmidt <randallschmidt1@gmail.com>
2020-09-07 12:50:24 -0400

    handle excessive newlines in deck description

--- common/deck_validation.js
+++ common/deck_validation.js
@@ -4,6 +4,7 @@ const INSTRUCTIONS_MAX_LENGTH = 400;
 const IMAGE_QUESTION_MAX_LENGTH = 20;
 const TEXT_QUESTION_MAX_LENGTH = 400;
 const DESCRIPTION_MAX_LENGTH = 500;
+const MAX_NEWLINES_IN_DESCRIPTION = 8;
 const ANSWERS_TOTAL_MAX_LENGTH = 200;
 const COMMENT_MAX_LENGTH = 600;
 const MAX_CARDS = 10000;
@@ -61,6 +62,10 @@ function sanitizeDeckPreValidation(deck) {
 
   if (typeof deckCopy.name === typeof '') {
     deckCopy.name = deckCopy.name.trim();
+  }
+
+  if (typeof deckCopy.description === typeof '') {
+    deckCopy.description = deckCopy.description.trim();
   }
 
   for (let cardIndex = 0; cardIndex < deckCopy.cards.length; cardIndex += 1) {
@@ -192,6 +197,15 @@ function validateCards(cards) {
   return successValidationResult;
 }
 
+function countOccurrences(str, character) {
+  let count = 0;
+  for (let i = 0; i < str.length; ++i) {
+    count += str[i] === character ? 1 : 0;
+  }
+
+  return count;
+}
+
 function validateDeck(deck) {
   if (typeof deck !== typeof {}) {
     return createFailureValidationResult(NON_LINE_ERROR_LINE, 'Deck is not an object. Please report this error.');
@@ -214,7 +228,7 @@ function validateDeck(deck) {
   }
 
   if (deck.shortName.length < 1 || deck.shortName.length > SHORT_NAME_MAX_LENGTH) {
-    return createFailureValidationResult(NON_LINE_ERROR_LINE, `The deck's short name must be more than 0 and less than ${SHORT_NAME_MAX_LENGTH} characters long.`);
+    return createFailureValidationResult(NON_LINE_ERROR_LINE, `The deck's short name must be more than 0 and no more than ${SHORT_NAME_MAX_LENGTH} characters long.`);
   }
 
   if (!deck.shortName.match(SHORT_NAME_ALLOWED_CHARACTERS_REGEX)) {
@@ -234,11 +248,19 @@ function validateDeck(deck) {
   }
 
   if (deck.name.length < 1 || deck.name.length > FULL_NAME_MAX_LENGTH) {
-    return createFailureValidationResult(NON_LINE_ERROR_LINE, `The deck's full name must be more than 0 and less than ${FULL_NAME_MAX_LENGTH} characters long.`);
+    return createFailureValidationResult(NON_LINE_ERROR_LINE, `The deck's full name must be more than 0 and no more than ${FULL_NAME_MAX_LENGTH} characters long.`);
+  }
+
+  if (deck.name.includes('\n')) {
+    return createFailureValidationResult(NON_LINE_ERROR_LINE, `The deck's full name must not contain newlines.`);
   }
 
   if (deck.description.length > DESCRIPTION_MAX_LENGTH) {
-    return createFailureValidationResult(NON_LINE_ERROR_LINE, `The deck's description must be less than ${DESCRIPTION_MAX_LENGTH} characters long.`);
+    return createFailureValidationResult(NON_LINE_ERROR_LINE, `The deck's description must be no more than than ${DESCRIPTION_MAX_LENGTH} characters long.`);
+  }
+
+  if (countOccurrences(deck.description, '\n') > MAX_NEWLINES_IN_DESCRIPTION) {
+    return createFailureValidationResult(NON_LINE_ERROR_LINE, `The deck's description must have no more than ${MAX_NEWLINES_IN_DESCRIPTION} newlines.`);
   }
 
   return validateCards(deck.cards);
