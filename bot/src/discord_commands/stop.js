ab55404c460835c5933927bd562aeb383fbc6dde
Randall Schmidt <randallschmidt1@gmail.com>
2020-09-19 10:26:51 -0400

    fix stopping quiz as server admin

--- bot/src/discord_commands/stop.js
+++ bot/src/discord_commands/stop.js
@@ -4,15 +4,16 @@ const quizManager = require('./../common/quiz/manager.js');
 const shiritoriManager = require('kotoba-node-common').shiritori;
 
 module.exports = {
-  commandAliases: ['endquiz', 'endtest', 'stop', 'quit', 'ｑｓ'],
+  commandAliases: ['endquiz', 'end', 'stop', 'quit', 'cancel', 'ｑｓ'],
   canBeChannelRestricted: false,
   uniqueId: 'stop',
+  attachIsServerAdmin: true,
   action(bot, msg) {
     const locationId = msg.channel.id;
     const userId = msg.author.id;
 
     return Promise.all([
-      quizManager.stopQuiz(locationId, userId),
+      quizManager.stopQuiz(locationId, userId, msg.authorIsServerAdmin),
       shiritoriManager.stopGame(locationId, userId),
     ]);
   },
