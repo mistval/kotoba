dcf16cd8040474c3e27c708b24e263a83f1f7bc0
Ernesto Pacheco Merino <ernespa@hotmail.com>
2020-11-14 15:30:19 +0100

    Code clarification and response messages

--- bot/src/discord_commands/schedule.js
+++ bot/src/discord_commands/schedule.js
@@ -17,7 +17,7 @@ function randomAction(msg, suffix, monochrome) {
   );
 }
 
-function setTimer(id, msg, command, monochrome, freq, startTime) {
+function setTimer(id, msg, level, monochrome, freq, startTime) {
   let nextTime = 0;
   if (startTime) {
     nextTime = startTime.getTime() - (new Date()).getTime();
@@ -30,8 +30,8 @@ function setTimer(id, msg, command, monochrome, freq, startTime) {
   let index = intervals.findIndex(i => i.id === id);
   if (index !== -1) {
     intervals[index].interval = setTimeout(() => {
-      randomAction(msg, command, monochrome);
-      setTimer(id, msg, command, monochrome, freq);
+      randomAction(msg, level, monochrome);
+      setTimer(id, msg, level, monochrome, freq);
     }, nextTime)
   }
 }
@@ -40,18 +40,21 @@ async function setSchedule(suffix, msg, monochrome) {
   let freq = '';
   let freqType = '';
   let start = '';
-  let command = '';
+  let level = '';
   let suffixArray = suffix.split(" ");
   if (suffixArray.length === 1 && suffixArray[0] === 'clear') {
     intervals.forEach(i => clearTimeout(i.interval));
     intervals = [];
+    msg.channel.createMessage('All scheduled commands have been cleared.');
   } else if (suffixArray.length === 1 && suffixArray[0] === 'list') {
     if (intervals.length > 0) {
-      intervals.forEach(i => msg.channel.createMessage('ID: ' + i.id +
-                                                      '\nChannel: ' + i.channel +
-                                                      '\nFrequency: ' + i.frequency +
-                                                      '\nStart time: ' + i.start +
-                                                      '\nCommand: ' + i.command));
+      let list = '';
+      intervals.forEach(i => list = (list === '' ? '' : list + '\n') + 'ID: ' + i.id +
+                                            ', Channel: ' + i.channel +
+                                            ', Frequency: ' + i.frequency +
+                                            ', Start time: ' + i.start +
+                                            ', Level: ' + i.level);
+      msg.channel.createMessage(list);
     } else {
       msg.channel.createMessage('List is empty');
     }
@@ -60,9 +63,12 @@ async function setSchedule(suffix, msg, monochrome) {
     if (index !== -1) {
       clearTimeout(intervals[index].interval);
       intervals.splice(index, 1);
+      msg.channel.createMessage('Scheduled command stopped succesfully.');
+    } else {
+      msg.channel.createMessage('Error: Scheduled command with ID ' + suffixArray[1] + ' not found.');
     }
-  } else if (suffixArray.length < 3) {
-    msg.channel.createMessage('Error: You must specify frequency in hours (or use \'daily\' or \'weekly\'), and a starting time (or use \'now\')');
+  } else if (suffixArray.length !== 3) {
+    msg.channel.createMessage('Error: You must specify frequency in hours (or use \'daily\' or \'weekly\'), and a starting time (or use \'now\'). Frequency formats consist of number and unit. Valid units are \'s\', \'m\', \'h\', \'d\', \'w\'. Starting time valid fotmat is \'hh:mm\'.');
   } else {
     switch (suffixArray[0]) {
       case 'daily':
@@ -97,7 +103,7 @@ async function setSchedule(suffix, msg, monochrome) {
               break;
           }
         } else {
-          msg.channel.createMessage('Error: You must specify frequency');
+          msg.channel.createMessage('Error: You must specify frequency. Frequency formats consist of number and unit. Valid units are \'s\', \'m\', \'h\', \'d\', \'w\'.');
           return;
         }
         break;
@@ -119,21 +125,21 @@ async function setSchedule(suffix, msg, monochrome) {
           start.setSeconds(0);
           start.setMilliseconds(0);
         } else {
-          msg.channel.createMessage('Error: Start time must be in format hh:mm');
+          msg.channel.createMessage('Error: Start time must be in format hh:mm.');
           return;
         }
         break;
     }
-    command = suffix.substring(suffixArray[0].length + 1 + suffixArray[1].length + 1);
+    level = suffixArray[2];
     let id = intervals.length > 0 ? intervals[intervals.length-1].id + 1 : 0;
     intervals.push({
       id: id,
       channel: msg.channel.name,
       frequency: suffixArray[0],
       start: start.toString(),
-      command: command,
+      level: level,
       interval: setTimeout(() => {
-        setTimer(id, msg, command, monochrome, freq, start);
+        setTimer(id, msg, level, monochrome, freq, start);
       }, 0)
     });
   }
@@ -144,8 +150,8 @@ module.exports = {
   canBeChannelRestricted: true,
   uniqueId: 'schedule',
   requiredSettings: 'dictionary/display_mode',
-  shortDescription: 'Schedule the \'random\' command to run periodically.',
-  longDescription: 'Schedule the \'random\' command to run periodically. You must specify frequency (or use \'daily\' or \'weekly\'), and a starting time (or use \'now\'). Frequency formats consist of number and unit. Valid units are \'s\', \'m\', \'h\', \'d\', \'w\'. Starting time valid fotmat is \'hh:mm\'.',
+  shortDescription: 'Schedule the \'random\' command to run periodically. This command can be only used by a server Admin.',
+  longDescription: 'Schedule the \'random\' command to run periodically. This command can be only used by a server Admin. You must specify frequency (or use \'daily\' or \'weekly\'), and a starting time (or use \'now\'). Frequency formats consist of number and unit. Valid units are \'s\', \'m\', \'h\', \'d\', \'w\'. Starting time valid fotmat is \'hh:mm\'. You can see all the scheduled commands by using \'<prefix>schedule list\'. You can clear all scheduled commands by using \'<prefix>schedule clear\' or cancel one by using \'<prefix>schedule stop ID\'.',
   usageExample: '<prefix>schedule 1h now N3, <prefix>schedule daily 18:00 2k',
   requiredBotPermissions: [Permissions.embedLinks, Permissions.sendMessages],
   action(bot, msg, suffix, monochrome) {
