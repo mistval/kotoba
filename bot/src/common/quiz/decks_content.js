ef688399dba03c1b92c04ecee03047fed3bd4fdb
Randall Schmidt <randallschmidt1@gmail.com>
2021-01-02 09:23:44 -0500

    add buddha quiz

--- bot/src/common/quiz/decks_content.js
+++ bot/src/common/quiz/decks_content.js
@@ -9,7 +9,7 @@ const categoryFields = [
   {name: 'JLPT Decks', value: 'N5 N4 N3 N2 N1 gN5 gN4 gN3 gN2 gN1 vuN3 vuN2 vuN1 k_N5 k_N4 k_N3 k_N2 k_N1 N5m N4m N3m N2m N1m'},
   {name: 'Kanken Decks', value: '10k 9k 8k 7k 6k 5k 4k 3k j2k 2k j1k 1k 10km 9km 8km 7km 6km 5km 4km 3km j2km 2km j1km 1km'},
   {name: 'Listening Decks (must be in voice channel)', value: 'LN3 LN2 LN1 lvN5 lvN4 lvN3 lvN2 lvN1 lv10k lv9k lv8k lv7k lv6k lv5k lv4k lv3k lvj2k lv2k lvj1k lv1k'},
-  {name: '用語 Decks', value: 'suugaku pasokon rikagaku igaku shinrigaku keizai houritsu kenchiku nature animals birds bugs fish plants vegetables'},
+  {name: '用語 Decks', value: 'suugaku pasokon rikagaku igaku shinrigaku keizai houritsu kenchiku buddha nature animals birds bugs fish plants vegetables'},
   {name: 'Japanese Place Name Decks', value: 'places_full tokyo osaka hokkaido aichi kanagawa (say **<prefix>quiz places** to see the rest)'},
   {name: 'Japanese Station Name Decks', value: 'stations_full stations_tokyo stations_osaka (say **<prefix>quiz stations** to see the rest)'},
   {name: 'Japanese Misc. Decks', value: 'hiragana katakana kanawords rtk_vocab kotowaza common common_nojlpt k33 hard haard insane hentaigana kklc ranobe numbers yojijukugo kirakira dqn sao jsyn jsyn_full jptrivia kokuji onomato 擬音語 radicals jouyou images jpdefs myouji namae onago seiyuu cities prefectures countries emperors'},
