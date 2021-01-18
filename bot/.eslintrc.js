129bb07c1aa1352ad3778a72b4fe2df308d5636a
Randall Schmidt <randallschmidt1@gmail.com>
2021-01-18 13:41:16 -0500

    allow for-of loop

--- bot/.eslintrc.js
+++ bot/.eslintrc.js
@@ -3,5 +3,20 @@ module.exports = {
     "rules": {
       "class-methods-use-this": [2, { "exceptMethods": ['prepareData'] }],
       "no-console": "off",
+      'no-restricted-syntax': [
+        'error',
+        {
+          selector: 'ForInStatement',
+          message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+        },
+        {
+          selector: 'LabeledStatement',
+          message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+        },
+        {
+          selector: 'WithStatement',
+          message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+        },
+      ],
     }
 };
