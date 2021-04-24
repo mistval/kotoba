3e70b5a6cd715138b302ad3b4c1a1855fe2cbde4
Randall Schmidt <randallschmidt1@gmail.com>
2021-04-24 13:49:18 -0400

    exclude game reports from backups

--- backup/backup.sh
+++ backup/backup.sh
@@ -10,7 +10,7 @@ cp -f ./config/gcloud_key.json ./backup
 
 rm -rf ./backup_out
 mkdir -p ./backup_out/user_data/bot
-docker-compose exec -T mongo_readwrite mongodump --archive=./mongo_dump.archive
+docker-compose exec -T mongo_readwrite mongodump --archive=./mongo_dump.archive --excludeCollection=gamereports
 docker-compose exec -T mongo_readwrite cat ./mongo_dump.archive > ./backup_out/mongo_dump.archive
 
 cp -r ./user_data/bot/monochrome-persistence ./backup_out/user_data/bot
