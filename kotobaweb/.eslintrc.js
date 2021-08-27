4fa07a95984d3881b8967f82348ed7c4c55fb852
Randall Schmidt <randallschmidt1@gmail.com>
2021-08-27 10:59:23 -0400

    disable unhelpful eslint rules

--- kotobaweb/.eslintrc.js
+++ kotobaweb/.eslintrc.js
@@ -10,6 +10,9 @@ module.exports = {
       'jsx-a11y/label-has-for': 0, // This rule has a bug, see https://github.com/evcohen/eslint-plugin-jsx-a11y/issues/455
       'jsx-a11y/label-has-associated-control': 0, // Nesting the input within the label seems to break the input with MD boostrap.
       'jsx-a11y/click-events-have-key-events': 0,
-      "react/prop-types": [2, { ignore: ['history', 'formikArgs'] }], // History comes from React (withRouter), formikArgs comes from formik
+      'react/prop-types': 'off',
+      'react/destructuring-assignment': 'off',
+      'max-len': 'off',
+      'react/no-unescaped-entities': 'off',
     },
 };
