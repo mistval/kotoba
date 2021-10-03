67b087889f0a19dd9ecd5244cf89f13f79be56ef
Randall Schmidt <randallschmidt1@gmail.com>
2021-10-03 09:21:40 -0400

    update backup scripts

--- backup/init.sh
+++ backup/init.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
-docker pull google/cloud-sdk
-
-echo "0 9,21 * * * root sh $parent_path/backup.sh" > /etc/cron.d/kotoba_backup
+echo "0 9,21 * * * root /root/kotoba/backup/backup.sh" > /etc/cron.d/kotoba_backup
