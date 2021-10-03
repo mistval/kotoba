20dabcba317730ed98e27f80d3b060963c320df1
Randall Schmidt <randallschmidt1@gmail.com>
2021-10-03 08:39:26 -0400

    update to mongoose 6

--- node-common/mongodb.js
+++ node-common/mongodb.js
@@ -3,15 +3,7 @@ const mongoose = require('mongoose');
 const DB_NAME = 'kotoba';
 const DB_HOST = process.env.MONGO_HOST || 'localhost:27017';
 const DB_CONNECTION_STRING = `mongodb://${DB_HOST}/${DB_NAME}`;
-const connection = mongoose.createConnection(
-  DB_CONNECTION_STRING,
-  {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  },
-);
+const connection = mongoose.createConnection(DB_CONNECTION_STRING);
 
 module.exports = {
   connection,
