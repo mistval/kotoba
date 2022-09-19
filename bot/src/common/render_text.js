454166b5e3ce6a4ad1b1b1dc50dd6bf77688517d
Darius Jahandarie <djahandarie@gmail.com>
2022-09-19 13:26:21 -0400

    add antiocr text rendering effect for quizzes

--- bot/src/common/render_text.js
+++ bot/src/common/render_text.js
@@ -2,13 +2,21 @@ const Canvas = require('canvas');
 require('canvas-5-polyfill');
 const { fontHelper } = require('./globals.js');
 
-const TOP_PADDING_IN_PIXELS = 6;
-const BOTTOM_PADDING_IN_PIXELS = 6;
-const BASE_LEFT_PADDING_IN_PIXELS = 6;
-const BASE_RIGHT_PADDING_IN_PIXELS = 6;
-const TOTAL_VERTICAL_PADDING_IN_PIXELS = TOP_PADDING_IN_PIXELS + BOTTOM_PADDING_IN_PIXELS;
+function render(
+  text,
+  textColor = 'black',
+  backgroundColor = 'white',
+  fontSize = 96,
+  fontSetting = 'Meiryo',
+  allowFontFallback = true,
+  effect = 'none',
+) {
+  const TOP_PADDING_IN_PIXELS = effect == "antiocr" ? 30 : 6;
+  const BOTTOM_PADDING_IN_PIXELS = effect == "antiocr" ? 30 : 6;
+  const BASE_LEFT_PADDING_IN_PIXELS = effect == "antiocr" ? 30 : 6;
+  const BASE_RIGHT_PADDING_IN_PIXELS = effect == "antiocr" ? 30 : 6;
+  const TOTAL_VERTICAL_PADDING_IN_PIXELS = TOP_PADDING_IN_PIXELS + BOTTOM_PADDING_IN_PIXELS;
 
-function render(text, textColor = 'black', backgroundColor = 'white', fontSize = 96, fontSetting = 'Yu Mincho', allowFontFallback = true) {
   const { fontFamily } = fontHelper.getFontForAlias(fontSetting);
   const coercedFont = allowFontFallback
     ? fontHelper.coerceFontFamily(fontFamily, text)
@@ -32,13 +40,49 @@ function render(text, textColor = 'black', backgroundColor = 'white', fontSize =
   ctx.fillStyle = backgroundColor;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  ctx.fillStyle = textColor;
+
+  if (effect == "antiocr") {
+    ctx.fillStyle = "#333333"
+    ctx.font = `bold 30px Arial`;
+    ctx.shadowColor = "black";
+    ctx.shadowBlur = 6;
+    ctx.shadowOffsetX = 6;
+    ctx.shadowOffsetY = 6;
+    const ocrFontHeight = 28;
+    for (var h = 0; h < canvas.height + ocrFontHeight; h += ocrFontHeight) {
+      ctx.fillText(
+        "OCR ".repeat(50),
+        0,
+        h
+      )
+    }
+    ctx.fillStyle = ctx.createLinearGradient(0, 0, canvas.width, 0);
+    ctx.fillStyle.addColorStop(0, "red");
+    ctx.fillStyle.addColorStop(0.5, "blue");
+    ctx.fillStyle.addColorStop(1.0, "magenta");
+    ctx.globalAlpha = 0.8;
+  } else {
+    ctx.fillStyle = textColor;
+  }
   ctx.font = `${fontSize}px ${coercedFont}`;
   ctx.fillText(
     text,
     leftPaddingInPixels,
     measurements.actualBoundingBoxAscent + TOP_PADDING_IN_PIXELS,
   );
+  if (effect == "antiocr") {
+    ctx.strokeStyle = ctx.createLinearGradient(0, 0, canvas.width, 0);
+    ctx.strokeStyle.addColorStop(0, "blue");
+    ctx.strokeStyle.addColorStop(0.5, "orange");
+    ctx.strokeStyle.addColorStop(1.0, "gray");
+    ctx.lineWidth = 2;
+    ctx.strokeText(
+      text,
+      leftPaddingInPixels,
+      measurements.actualBoundingBoxAscent + TOP_PADDING_IN_PIXELS,
+    );
+  }
+
 
   return new Promise((fulfill, reject) => {
     const bufferOptions = { compressionLevel: 5, filters: canvas.PNG_FILTER_NONE };
